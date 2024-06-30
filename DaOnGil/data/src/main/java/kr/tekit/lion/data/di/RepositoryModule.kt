33d8ce7eaae023a885b8e260d8773e99cf20ecef
chanho0908 <uni070@naver.com>
2024-06-30 09:40:41 +0900

    [Feat] #1 무장애 API Repository Module

---
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/di/RepositoryModule.kt
@@ -0,0 +1,21 @@
+package kr.tekit.lion.data.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kr.tekit.lion.data.repository.AreaCodeRepositoryImpl
+import kr.tekit.lion.data.repository.KorWithRepositoryImpl
+import kr.tekit.lion.data.repository.SigunguCodeRepositoryImpl
+import kr.tekit.lion.domain.repository.AreaCodeRepository
+import kr.tekit.lion.domain.repository.KorWithRepository
+import kr.tekit.lion.domain.repository.SigunguCodeRepository
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+
+    @Binds
+    abstract fun bindKorWithRepository(korWithRepositoryImpl: KorWithRepositoryImpl): KorWithRepository
+}
