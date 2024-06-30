1f0230fe7b270c5b3643b3db6f5804564a4f6927
chanho0908 <uni070@naver.com>
2024-06-30 09:39:43 +0900

    [Feat] #1 지역 코드 API Hilt Module

---
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/di/NetworkModule.kt
@@ -0,0 +1,39 @@
+package kr.tekit.lion.data.di
+
+import android.util.Log
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kr.tekit.lion.data.service.KorWithService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY)
+            ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideKorWithService(okHttpClient: OkHttpClient): KorWithService =
+        Retrofit.Builder()
+            .baseUrl("https://apis.data.go.kr/B551011/KorWithService1/")
+            .addConverterFactory(MoshiConverterFactory.create().asLenient())
+            .client(okHttpClient)
+            .build()
+            .create()
+}
