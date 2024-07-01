38488c8cf8f72e658ff0d4f129e19f5ecbdd7a99
chanho0908 <uni070@naver.com>
2024-07-01 23:26:08 +0900

    [Feat] #1 지역이름 풀 네임 매핑 클래스 추가

--- DaOnGil/data/src/main/java/kr/tekit/lion/data/repository/AreaCodeRepositoryImpl.kt
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/repository/AreaCodeRepositoryImpl.kt
@@ -3,6 +3,7 @@ package kr.tekit.lion.data.repository
 import kr.tekit.lion.daongil.data.dto.local.toDomainModel
 import kr.tekit.lion.daongil.data.dto.local.toEntity
 import kr.tekit.lion.data.datasource.AreaCodeDataSource
+import kr.tekit.lion.data.mapper.toFullAreaName
 import kr.tekit.lion.domain.model.AreaCode
 import kr.tekit.lion.domain.repository.AreaCodeRepository
 import javax.inject.Inject
@@ -23,17 +24,8 @@ class AreaCodeRepositoryImpl @Inject constructor(
 
     override suspend fun addAreaCodeInfo(areaCodeList: List<AreaCode>) {
         areaCodeDataSource.addAreaCodeInfoList(areaCodeList.map {
-            when(it.name){
-                "서울" -> AreaCode(it.code, "서울특별시").toEntity()
-                "인천" -> AreaCode(it.code, "인천광역시").toEntity()
-                "부산" -> AreaCode(it.code, "부산광역시").toEntity()
-                "대전" -> AreaCode(it.code, "대전광역시").toEntity()
-                "대구" -> AreaCode(it.code, "대구광역시").toEntity()
-                "광주" -> AreaCode(it.code, "광주광역시").toEntity()
-                "울산" -> AreaCode(it.code, "울산광역시").toEntity()
-                "제주도" -> AreaCode(it.code, "제주특별자치도").toEntity()
-                else -> it.toEntity()
+                AreaCode(it.code, it.name.toFullAreaName()).toEntity()
             }
-        })
+        )
     }
 }
