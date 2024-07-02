200dde6acaebc008742a29ee9274da10d45c8050
chanho0908 <uni070@naver.com>
2024-07-02 21:45:49 +0900

    [Feat] #3 확장함수 이전

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/ext/ContextExt.kt
@@ -0,0 +1,100 @@
+package kr.tekit.lion.presentation.ext
+
+import android.content.ContentUris
+import android.content.Context
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.provider.Settings
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+fun Context.showSoftInput(view: View) {
+    val inputMethodManger =
+        this.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManger.showSoftInput(view, 0)
+}
+
+fun Context.showPermissionSnackBar(view: View) {
+    Snackbar.make(
+        view, "권한이 거부 되었습니다. 설정(앱 정보)에서 권한을 확인해 주세요.",
+        Snackbar.LENGTH_SHORT
+    ).setAction("확인") {
+        //설정 화면으로 이동
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val packageName = this.packageName
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+
+        this.startActivity(intent)
+
+    }.show()
+}
+
+fun Context.toAbsolutePath(uri: Uri): String? {
+    if (DocumentsContract.isDocumentUri(this, uri)) {
+        when {
+            uri.isExternalStorageDocument() -> {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":").toTypedArray()
+                val type = split[0]
+
+                if ("primary".equals(type, ignoreCase = true)) {
+                    return "${getExternalFilesDir(null)?.absolutePath}/${split[1]}"
+                }
+            }
+
+            uri.isDownloadsDocument() -> {
+                val id = DocumentsContract.getDocumentId(uri)
+                val contentUri = ContentUris.withAppendedId(
+                    Uri.parse("content://downloads/public_downloads"), id.toLong()
+                )
+                return getDataColumn(contentUri, null, null)
+            }
+
+            uri.isMediaDocument() -> {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":").toTypedArray()
+
+                val contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(split[1])
+
+                return getDataColumn(contentUri, selection, selectionArgs)
+            }
+        }
+    }
+    // MediaStore
+    else if ("content".equals(uri.scheme, ignoreCase = true)) {
+        return getDataColumn(uri, null, null)
+    }
+    // File
+    else if ("file".equals(uri.scheme, ignoreCase = true)) {
+        return uri.path
+    }
+
+    return null
+}
+
+fun Context.getDataColumn(uri: Uri?, selection: String?, selectionArgs: Array<String>?): String? {
+    var cursor: Cursor? = null
+    val column = "_data"
+    val projection = arrayOf(column)
+
+    try {
+        cursor =
+            uri?.let { this.contentResolver.query(it, projection, selection, selectionArgs, null) }
+        if (cursor != null && cursor.moveToFirst()) {
+            val index = cursor.getColumnIndexOrThrow(column)
+            return cursor.getString(index)
+        }
+    } finally {
+        cursor?.close()
+    }
+    return null
+}
