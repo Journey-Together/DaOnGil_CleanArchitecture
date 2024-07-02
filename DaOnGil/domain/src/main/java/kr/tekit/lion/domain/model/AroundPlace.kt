9325197d35449bb94c11857396b0211b6317b095
chanho0908 <uni070@naver.com>
2024-07-02 21:47:31 +0900

    [Feat] #3 관광지 Domain Model

---
+++ DaOnGil/domain/src/main/java/kr/tekit/lion/domain/model/AroundPlace.kt
@@ -0,0 +1,9 @@
+package kr.tekit.lion.domain.model
+
+data class AroundPlace (
+    val address: String,
+    val disability: List<String>,
+    val image: String,
+    val name: String,
+    val placeId: Int
+)
