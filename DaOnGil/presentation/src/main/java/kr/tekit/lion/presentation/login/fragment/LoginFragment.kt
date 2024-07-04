45d2860e4142572cb9c21d7a67fa93b128866e2c
chanho0908 <uni070@naver.com>
2024-07-05 07:42:47 +0900

    [BUG] #7 카카오 로그인 오류 테스트
    + 카카오 로그인시 로그인 요청이 두 번 요청되는 문제 원인 파악을 위해
    suspendCoroutine으로 만들었던 카카오 로그인 로직을 샘플 코드로 변경 후 테스트 합니다

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/login/fragment/LoginFragment.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/login/fragment/LoginFragment.kt
@@ -61,11 +61,41 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         }
     }
 
-    private fun kakaoLogin() = repeatOnViewStarted {
-        UserApiClient.login(requireContext())
-            .onSuccess {
-                viewModel.onCompleteLogIn("KAKAO", it.accessToken)
+//    private fun kakaoLogin() = repeatOnViewStarted {
+//        UserApiClient.login(requireContext())
+//            .onSuccess {
+//                viewModel.onCompleteLogIn("KAKAO", it.accessToken)
+//            }
+//    }
+
+    private fun kakaoLogin(){
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+
+            } else if (token != null) {
+
             }
+        }
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+            UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+                if (error != null) {
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
+                } else if (token != null) {
+                    viewModel.onCompleteLogIn("KAKAO", token.accessToken)
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
+        }
     }
 
     private suspend fun UserApiClient.Companion.login(context: Context): Result<OAuthToken> = runCatching {
