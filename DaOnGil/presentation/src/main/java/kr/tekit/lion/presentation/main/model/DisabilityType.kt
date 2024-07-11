f5370bf204cc4959e30283bea56ff1dfe60660dd
chanho0908 <uni070@naver.com>
2024-07-11 15:54:21 +0900

    [Refactor] #9 장애 유형 Model이 FilterCode도 갖도록 수정

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/model/DisabilityType.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/model/DisabilityType.kt
@@ -1,9 +1,29 @@
 package kr.tekit.lion.presentation.main.model
 
-sealed class DisabilityType(val type: Long) {
-    data object PhysicalDisability : DisabilityType(1)
-    data object VisualImpairment : DisabilityType(2)
-    data object HearingImpairment : DisabilityType(3)
-    data object InfantFamily : DisabilityType(4)
-    data object ElderlyPeople : DisabilityType(5)
+sealed class DisabilityType(val code: Long) {
+    abstract val filterCodes: Set<Long>
+}
+data object PhysicalDisability : DisabilityType(1) {
+    override val filterCodes: Set<Long>
+        get() = setOf(Parking.code, Wheelchair.code, Elevator.code, RestRoom.code, Seat.code)
+}
+
+data object VisualImpairment : DisabilityType(2) {
+    override val filterCodes: Set<Long>
+        get() = setOf(Braileblock.code, HelpDog.code, Guide.code, AudioGuide.code)
+}
+
+data object HearingImpairment : DisabilityType(3) {
+    override val filterCodes: Set<Long>
+        get() = setOf(SignGuide.code, VideoGuide.code)
+}
+
+data object InfantFamily : DisabilityType(4) {
+    override val filterCodes: Set<Long>
+        get() = setOf(Stroller.code, LactationRoom.code, BabySpareChair.code)
+}
+
+data object ElderlyPeople : DisabilityType(5) {
+    override val filterCodes: Set<Long>
+        get() = setOf(WheelchairLent.code)
 }
