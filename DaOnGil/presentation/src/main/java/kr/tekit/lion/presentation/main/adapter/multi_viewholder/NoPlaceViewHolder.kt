1f9a6b23f86fe15858e4604885f6f251c075a496
chanho0908 <uni070@naver.com>
2024-07-11 16:31:36 +0900

    [#7] 검색 결과 없을 때 ViewHolder

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/multi_viewholder/NoPlaceViewHolder.kt
@@ -0,0 +1,6 @@
+package kr.tekit.lion.presentation.main.adapter.multi_viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import kr.tekit.lion.presentation.databinding.ItemNoPlaceBinding
+
+class NoPlaceViewHolder(binding: ItemNoPlaceBinding) : RecyclerView.ViewHolder(binding.root)
