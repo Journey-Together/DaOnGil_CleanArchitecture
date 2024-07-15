8dc48762fe374d117b5121e37799021213081e28
chanho0908 <uni070@naver.com>
2024-07-15 17:29:55 +0900

    [Feat] #9 리사이클러뷰에서 에러메시지 출력하도록 수정

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/multi_viewholder/NoPlaceViewHolder.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/multi_viewholder/NoPlaceViewHolder.kt
@@ -3,4 +3,8 @@ package kr.tekit.lion.presentation.main.adapter.multi_viewholder
 import androidx.recyclerview.widget.RecyclerView
 import kr.tekit.lion.presentation.databinding.ItemNoPlaceBinding
 
-class NoPlaceViewHolder(binding: ItemNoPlaceBinding) : RecyclerView.ViewHolder(binding.root)
+class NoPlaceViewHolder(private val binding: ItemNoPlaceBinding) : RecyclerView.ViewHolder(binding.root){
+    fun bind(msg: String){
+        binding.textMsg.text = msg
+    }
+}
