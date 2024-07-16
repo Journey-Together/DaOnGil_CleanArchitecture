b83d0cd7d9bc08376ecbd36f478e6ca86b164489
chanho0908 <uni070@naver.com>
2024-07-16 16:32:46 +0900

    [Refactor] #13 관광지 리스트 검색 api 변경으로 인한 default 옵션 추가

--- DaOnGil/domain/src/main/java/kr/tekit/lion/domain/model/ListSearchOption.kt
+++ DaOnGil/domain/src/main/java/kr/tekit/lion/domain/model/ListSearchOption.kt
@@ -7,8 +7,8 @@ data class ListSearchOption (
     val size: Int,
     val page: Int,
     val query: String? = null,
-    val disabilityType: TreeSet<Long>? = null,
-    val detailFilter: TreeSet<Long>? = null,
+    val disabilityType: TreeSet<Long>? = TreeSet<Long>(setOf(1)),
+    val detailFilter: TreeSet<Long>? = TreeSet<Long>(setOf(1, 6, 7, 8, 9)),
     val areaCode: String? = null,
     val sigunguCode: String? = null,
     val arrange: String
