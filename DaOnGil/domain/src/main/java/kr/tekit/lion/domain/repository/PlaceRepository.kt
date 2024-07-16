0dac0050dcec3bed2b1a60e1869e6839eea6a078
chanho0908 <uni070@naver.com>
2024-07-16 16:31:41 +0900

    [Feat] #13 관광지 지도 검색 Repository

--- DaOnGil/domain/src/main/java/kr/tekit/lion/domain/repository/PlaceRepository.kt
+++ DaOnGil/domain/src/main/java/kr/tekit/lion/domain/repository/PlaceRepository.kt
@@ -1,9 +1,15 @@
 package kr.tekit.lion.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kr.tekit.lion.domain.model.ListSearchOption
 import kr.tekit.lion.domain.model.ListSearchResultList
+import kr.tekit.lion.domain.model.MapSearchOption
+import kr.tekit.lion.domain.model.Place
 import kr.tekit.lion.domain.model.Result
 
 interface PlaceRepository {
     suspend fun getSearchPlaceResultByList(request: ListSearchOption): Result<ListSearchResultList>
+
+    fun getSearchPlaceResultByMap(request: MapSearchOption): Flow<List<Place>>
 }
