c0bde8182debc761621442ef77d2a20b6e75a989
chanho0908 <uni070@naver.com>
2024-07-19 21:10:24 +0900

    [Feat] #13 Fragment 교체 방식 변경

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/fragment/SearchPlaceMainFragment.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/fragment/SearchPlaceMainFragment.kt
@@ -1,44 +1,47 @@
 package kr.tekit.lion.presentation.main.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.google.android.material.tabs.TabLayout
 import dagger.hilt.android.AndroidEntryPoint
-import kr.tekit.lion.domain.model.ConnectError
-import kr.tekit.lion.domain.model.HttpError
-import kr.tekit.lion.domain.model.TimeoutError
-import kr.tekit.lion.domain.model.UnknownHostError
 import kr.tekit.lion.presentation.R
 import kr.tekit.lion.presentation.databinding.FragmentSearchPlaceMainBinding
 import kr.tekit.lion.presentation.ext.repeatOnViewStarted
 import kr.tekit.lion.presentation.main.model.Category
 import kr.tekit.lion.presentation.main.model.ScreenState
 import kr.tekit.lion.presentation.main.vm.SearchMainViewModel
-import retrofit2.HttpException
-import java.net.ConnectException
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
 
 @AndroidEntryPoint
 class SearchPlaceMainFragment : Fragment(R.layout.fragment_search_place_main) {
     private val viewModel: SearchMainViewModel by viewModels()
+    private lateinit var listFragment: SearchListMainFragment
+    private lateinit var mapFragment: SearchMapFragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSearchPlaceMainBinding.bind(view)
+
+        // 프래그먼트 초기화 및 추가
+        listFragment = SearchListMainFragment()
+        mapFragment = SearchMapFragment()
+
+        childFragmentManager.beginTransaction().apply {
+            add(R.id.fragmentContainerView, listFragment, ScreenState.List.name)
+            add(R.id.fragmentContainerView, mapFragment, ScreenState.Map.name)
+            hide(mapFragment)
+            commit()
+        }
 
         with(binding) {
             tabContainer.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab) {
                     // 탭이 선택되었을 때 수행할 작업
                     when (tab.position) {
-                        0 -> viewModel.onSelectedTab(Category.PLACE.name)
-                        1 -> viewModel.onSelectedTab(Category.RESTAURANT.name)
-                        2 -> viewModel.onSelectedTab(Category.ROOM.name)
+                        0 -> viewModel.onSelectedTab(Category.PLACE)
+                        1 -> viewModel.onSelectedTab(Category.RESTAURANT)
+                        2 -> viewModel.onSelectedTab(Category.ROOM)
                     }
                 }
 
@@ -58,13 +61,15 @@ class SearchPlaceMainFragment : Fragment(R.layout.fragment_search_place_main) {
                 viewModel.screenState.collect {
                     when (it) {
                         ScreenState.Map -> {
-                            replaceFragment(SearchMapFragment())
+                            showFragment(mapFragment)
+                            hideFragment(listFragment)
                             modeSwitchBtn.setText(R.string.watching_list)
                             modeSwitchBtn.setIconResource(R.drawable.list_icon)
                         }
 
                         ScreenState.List -> {
-                            replaceFragment(SearchListMainFragment())
+                            showFragment(listFragment)
+                            hideFragment(mapFragment)
                             modeSwitchBtn.setText(R.string.watching_map)
                             modeSwitchBtn.setIconResource(R.drawable.map_icon)
                         }
@@ -74,9 +79,16 @@ class SearchPlaceMainFragment : Fragment(R.layout.fragment_search_place_main) {
         }
     }
 
-    private fun replaceFragment(fragment: Fragment) {
+    private fun showFragment(fragment: Fragment) {
         childFragmentManager.beginTransaction().apply {
-            replace(R.id.fragmentContainerView, fragment)
+            show(fragment)
+            commit()
+        }
+    }
+
+    private fun hideFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction().apply {
+            hide(fragment)
             commit()
         }
     }
