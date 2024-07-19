29b75bf9d3402e24366439e066cf76b3a3f06622
chanho0908 <uni070@naver.com>
2024-07-20 02:34:13 +0900

    [Chore] #13 코드 줄, 타입 수정

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/model/ScreenState.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/model/ScreenState.kt
@@ -1,6 +1,5 @@
 package kr.tekit.lion.presentation.main.model
 
-sealed class ScreenState {
-    data object List : ScreenState()
-    data object Map : ScreenState()
+enum class ScreenState {
+    List, Map
 }
