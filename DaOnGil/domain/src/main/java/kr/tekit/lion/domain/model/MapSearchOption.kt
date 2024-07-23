0b9daade84048177acdf44172e3732c5b7d0fb74
chanho0908 <uni070@naver.com>
2024-07-23 18:13:06 +0900

    [Chore] #13 기타 파일 삭제 및 코드 수정

--- DaOnGil/domain/src/main/java/kr/tekit/lion/domain/model/MapSearchOption.kt
+++ DaOnGil/domain/src/main/java/kr/tekit/lion/domain/model/MapSearchOption.kt
@@ -1,14 +1,12 @@
 package kr.tekit.lion.domain.model
 
-import java.util.TreeSet
-
 data class MapSearchOption (
     val category: String,
     val minX: Double,
     val maxX: Double,
     val minY: Double,
     val maxY: Double,
-    val disabilityType: TreeSet<Long>? = null,
-    val detailFilter: TreeSet<Long>? = null,
+    val disabilityType: List<Long>,
+    val detailFilter: List<Long>,
     val arrange: String
 )
