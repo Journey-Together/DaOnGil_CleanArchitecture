ea4707a95c313228978bd462af2785a730b68613
chanho0908 <uni070@naver.com>
2024-07-28 23:18:58 +0900

    [Feat] #19 네트워크 에러처리 수정(상속 제거)

--- DaOnGil/data/src/main/java/kr/tekit/lion/data/datasource/MemberDataSource.kt
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/datasource/MemberDataSource.kt
@@ -1,13 +1,14 @@
 package kr.tekit.lion.data.datasource
 
 import kr.tekit.lion.data.service.MemberService
+import kr.tekit.lion.data.common.execute
 import okhttp3.RequestBody
 import javax.inject.Inject
 
 class MemberDataSource @Inject constructor(
     private val memberService: MemberService
 ) {
-    suspend fun updateConcernType(requestBody: RequestBody) {
-        return memberService.updateConcernType(requestBody)
+    suspend fun updateConcernType(requestBody: RequestBody) = execute{
+        memberService.updateConcernType(requestBody)
     }
 }
