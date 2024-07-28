94550872d964f3fab53e6b9e17def4df2a3be7bd
chanho0908 <uni070@naver.com>
2024-07-28 23:21:06 +0900

    [Feat] #19 네트워크 에러처리 적용

--- DaOnGil/data/src/main/java/kr/tekit/lion/data/repository/MemberRepositoryImpl.kt
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/repository/MemberRepositoryImpl.kt
@@ -3,13 +3,14 @@ package kr.tekit.lion.data.repository
 import kr.tekit.lion.data.datasource.MemberDataSource
 import kr.tekit.lion.data.dto.remote.request.toRequestBody
 import kr.tekit.lion.domain.model.ConcernType
+import kr.tekit.lion.domain.model.Result
 import kr.tekit.lion.domain.repository.MemberRepository
 import javax.inject.Inject
 
 class MemberRepositoryImpl @Inject constructor(
     private val memberDataSource: MemberDataSource
 ): MemberRepository {
-    override suspend fun updateConcernType(requestBody: ConcernType) {
+    override suspend fun updateConcernType(requestBody: ConcernType): Result<Unit> {
         return memberDataSource.updateConcernType(requestBody.toRequestBody())
     }
 }
