72276a31aac936f00a52533e721c182bec0c8b07
chanho0908 <uni070@naver.com>
2024-08-08 00:33:19 +0900

    [Feat] #23 시군구 AutocompleteView ViewHolder

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/viewholder/SigunguViewHolder.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/viewholder/SigunguViewHolder.kt
@@ -1,4 +1,31 @@
 package kr.tekit.lion.presentation.main.adapter.viewholder
 
-class SigunguViewHolder {
+import android.widget.ArrayAdapter
+import androidx.core.widget.doAfterTextChanged
+import androidx.recyclerview.widget.RecyclerView
+import kr.tekit.lion.presentation.databinding.ItemListSearchSigunguBinding
+import kr.tekit.lion.presentation.main.model.SigunguModel
+
+class SigunguViewHolder(
+    private val binding: ItemListSearchSigunguBinding,
+    private val onSelectSigungu: (String) -> Unit,
+): RecyclerView.ViewHolder(binding.root) {
+    init {
+        binding.detailSelectedArea.doAfterTextChanged {
+            val sigungu = it.toString()
+            if (sigungu != "시/군/구") onSelectSigungu(sigungu)
+        }
+    }
+
+    fun bind(item: SigunguModel){
+        with(binding){
+            detailSelectedArea.setText("시/군/구")
+            val sigunguAdapter = ArrayAdapter(
+                root.context,
+                android.R.layout.simple_list_item_1,
+                item.sigungus
+            )
+            binding.detailSelectedArea.setAdapter(sigunguAdapter)
+        }
+    }
 }
