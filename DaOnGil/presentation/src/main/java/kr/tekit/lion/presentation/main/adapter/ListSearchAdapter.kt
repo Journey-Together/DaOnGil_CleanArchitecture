2da3f830fb0ef45d05071b210104330434f95de9
chanho0908 <uni070@naver.com>
2024-08-08 21:22:25 +0900

    [Feat] #23 notiifyDataSetChanged -> DiffUtil로 수정

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/ListSearchAdapter.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/ListSearchAdapter.kt
@@ -2,20 +2,25 @@ package kr.tekit.lion.presentation.main.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
 import kr.tekit.lion.presentation.R
 import kr.tekit.lion.presentation.databinding.ItemListSearchAreaBinding
 import kr.tekit.lion.presentation.databinding.ItemListSearchCategoryBinding
+import kr.tekit.lion.presentation.databinding.ItemListSearchSigunguBinding
+import kr.tekit.lion.presentation.databinding.ItemListSearchSortBinding
 import kr.tekit.lion.presentation.databinding.ItemNoPlaceBinding
 import kr.tekit.lion.presentation.databinding.ItemPlaceHighBinding
-import kr.tekit.lion.presentation.main.adapter.multi_viewholder.ListSearchAreaViewHolder
-import kr.tekit.lion.presentation.main.adapter.multi_viewholder.ListSearchCategoryViewHolder
-import kr.tekit.lion.presentation.main.adapter.multi_viewholder.NoPlaceViewHolder
-import kr.tekit.lion.presentation.main.adapter.multi_viewholder.PlaceHighViewHolder
+import kr.tekit.lion.presentation.main.adapter.viewholder.AreaViewHolder
+import kr.tekit.lion.presentation.main.adapter.viewholder.CategoryViewHolder
+import kr.tekit.lion.presentation.main.adapter.viewholder.NoPlaceViewHolder
+import kr.tekit.lion.presentation.main.adapter.viewholder.PlaceHighViewHolder
+import kr.tekit.lion.presentation.main.adapter.viewholder.SigunguViewHolder
+import kr.tekit.lion.presentation.main.adapter.viewholder.ItemCountViewHolder
 import kr.tekit.lion.presentation.main.model.AreaModel
 import kr.tekit.lion.presentation.main.model.CategoryModel
-import kr.tekit.lion.presentation.main.model.DisabilityType
 import kr.tekit.lion.presentation.main.model.ElderlyPeople
 import kr.tekit.lion.presentation.main.model.HearingImpairment
 import kr.tekit.lion.presentation.main.model.InfantFamily
@@ -23,6 +28,8 @@ import kr.tekit.lion.presentation.main.model.ListSearchUIModel
 import kr.tekit.lion.presentation.main.model.NoPlaceModel
 import kr.tekit.lion.presentation.main.model.PhysicalDisability
 import kr.tekit.lion.presentation.main.model.PlaceModel
+import kr.tekit.lion.presentation.main.model.SigunguModel
+import kr.tekit.lion.presentation.main.model.SortModel
 import kr.tekit.lion.presentation.main.model.VisualImpairment
 
 class ListSearchAdapter(
@@ -33,66 +40,17 @@ class ListSearchAdapter(
     private val onClickInfantFamily: (InfantFamily) -> Unit,
     private val onClickElderlyPeople: (ElderlyPeople) -> Unit,
     private val onSelectArea: (String) -> Unit,
-    private val onSelectSigungu: (String) -> Unit,
-    private val onClickSortByLatestBtn: (String) -> Unit,
-    private val onClickSortByPopularityBtn: (String) -> Unit,
-    private val onClickSortByLetterBtn: (String) -> Unit,
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    private val allDataList: MutableList<ListSearchUIModel> = mutableListOf()
-    private val areaList: MutableList<String> = mutableListOf()
-    private val sigunguList: MutableList<String> = mutableListOf()
-    private val optionState: MutableMap<DisabilityType, Int> = HashMap()
-
-    fun submitList(newData: Set<ListSearchUIModel>) {
-        val oldPlaceModels = allDataList.filterIsInstance<PlaceModel>()
-        if (oldPlaceModels.isNotEmpty()) {
-            allDataList.removeAll(oldPlaceModels)
-        }
-
-        val oldNoPlaceModels = allDataList.filterIsInstance<NoPlaceModel>()
-        val newPlaceModels = newData.filterIsInstance<PlaceModel>()
-
-        allDataList.addAll(newData)
-
-        if (newPlaceModels.isNotEmpty()) {
-            allDataList.removeAll(oldNoPlaceModels)
-        } else {
-            allDataList.removeAll(allDataList.filterIsInstance<NoPlaceModel>())
-            allDataList.add(NoPlaceModel())
-        }
-
-        notifyDataSetChanged()
-    }
-
-    fun submitErrorMessage(errorMessage: String) {
-        allDataList.clear()
-        allDataList.add(NoPlaceModel(errorMessage))
-        notifyDataSetChanged()
-    }
-
-    fun submitAreaList(areas: List<String>) {
-        areaList.addAll(areas)
-    }
-
-    fun submitSigunguList(list: List<String>) {
-        sigunguList.clear()
-        sigunguList.addAll(list)
-        notifyDataSetChanged()
-    }
-
-    fun modifyOptionState(option: Map<DisabilityType, Int>) {
-        optionState.clear()
-        optionState.putAll(option)
-        notifyDataSetChanged()
-    }
+    private val onSelectSigungu: (String) -> Unit
+) : ListAdapter<ListSearchUIModel, RecyclerView.ViewHolder>(DIFF_CALLBACK) {
 
     override fun getItemViewType(position: Int): Int {
-        return when (allDataList[position]) {
-            is CategoryModel -> CategoryModel.id
-            is PlaceModel -> PlaceModel().id
-            is AreaModel -> AreaModel.id
-            is NoPlaceModel -> NoPlaceModel().id
+        return when (getItem(position)) {
+            is CategoryModel -> R.layout.item_list_search_category
+            is PlaceModel -> R.layout.item_place_high
+            is AreaModel -> R.layout.item_list_search_area
+            is SigunguModel -> R.layout.item_list_search_sigungu
+            is SortModel -> R.layout.item_list_search_sort
+            is NoPlaceModel -> R.layout.item_no_place
         }
     }
 
@@ -101,7 +59,7 @@ class ListSearchAdapter(
         val v = layoutInflater.inflate(viewType, parent, false)
 
         return when (viewType) {
-            CategoryModel.id-> ListSearchCategoryViewHolder(
+            R.layout.item_list_search_category -> CategoryViewHolder(
                 ItemListSearchCategoryBinding.bind(v),
                 uiScope,
                 onClickPhysicalDisability,
@@ -110,46 +68,57 @@ class ListSearchAdapter(
                 onClickInfantFamily,
                 onClickElderlyPeople
             )
-
-            PlaceModel().id -> PlaceHighViewHolder(
+            R.layout.item_place_high -> PlaceHighViewHolder(
                 ItemPlaceHighBinding.bind(v)
             )
-
-            AreaModel.id -> ListSearchAreaViewHolder(
-                uiScope,
+            R.layout.item_list_search_area -> AreaViewHolder(
                 ItemListSearchAreaBinding.bind(v),
                 onSelectArea,
-                onSelectSigungu,
-                onClickSortByLatestBtn,
-                onClickSortByPopularityBtn,
-                onClickSortByLetterBtn
             )
-
-            NoPlaceModel().id -> NoPlaceViewHolder(
-                ItemNoPlaceBinding.bind(v)
+            R.layout.item_list_search_sort -> ItemCountViewHolder(
+                ItemListSearchSortBinding.bind(v)
             )
-
-            else -> throw IllegalArgumentException("Invalid view type")
+            R.layout.item_list_search_sigungu -> SigunguViewHolder(
+                ItemListSearchSigunguBinding.bind(v),
+                onSelectSigungu
+            )
+            R.layout.item_no_place -> NoPlaceViewHolder(ItemNoPlaceBinding.bind(v))
+            else -> throw IllegalArgumentException("Unknown View Type")
         }
     }
 
-    override fun getItemCount(): Int = allDataList.size
-
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val item = allDataList[position]
-        val place = allDataList.filterIsInstance<PlaceModel>()
-        val msg = allDataList.filterIsInstance<NoPlaceModel>()
-            .firstOrNull()?.msg ?: NoPlaceModel().msg
+        when (val item = getItem(position)) {
+            is CategoryModel -> {
+                (holder as CategoryViewHolder).bind(item)
+            }
+            is AreaModel -> {
+                (holder as AreaViewHolder).bind(item)
+            }
+            is SigunguModel -> {
+                (holder as SigunguViewHolder).bind(item)
+            }
+            is PlaceModel -> {
+                (holder as PlaceHighViewHolder).bind(item)
+            }
+            is SortModel -> {
+                (holder as ItemCountViewHolder).bind(item)
+            }
+            is NoPlaceModel -> {
+                (holder as NoPlaceViewHolder).bind(item.msg)
+            }
+        }
+    }
 
-        when (holder) {
-            is ListSearchCategoryViewHolder -> holder.bind(optionState)
-            is ListSearchAreaViewHolder -> holder.bind(
-                if (place.isEmpty()) 0 else place[0].itemCount,
-                areaList,
-                sigunguList
-            )
-            is PlaceHighViewHolder -> holder.bind(item as PlaceModel)
-            is NoPlaceViewHolder -> holder.bind(msg)
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ListSearchUIModel>() {
+            override fun areItemsTheSame(oldItem: ListSearchUIModel, newItem: ListSearchUIModel): Boolean {
+                return oldItem.uuid == newItem.uuid
+            }
+
+            override fun areContentsTheSame(oldItem: ListSearchUIModel, newItem: ListSearchUIModel): Boolean {
+                return oldItem == newItem
+            }
         }
     }
 }
