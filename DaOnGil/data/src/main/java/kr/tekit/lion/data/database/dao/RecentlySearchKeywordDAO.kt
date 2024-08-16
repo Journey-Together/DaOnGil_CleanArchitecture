ca860973be129f96d3ca4b3c956b92cc84fbe6a5
chanho0908 <uni070@naver.com>
2024-08-16 15:21:18 +0900

    [Feat] #23 Room DB 최근 검색어 저장 테이블 추가

---
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/database/dao/RecentlySearchKeywordDAO.kt
@@ -0,0 +1,22 @@
+package kr.tekit.lion.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import kr.tekit.lion.data.database.entity.RecentlySearchKeywordEntity
+
+@Dao
+interface RecentlySearchKeywordDAO {
+    @Query("SELECT * FROM recent_search_keyword_table")
+    fun readAllKeyword(): Flow<List<RecentlySearchKeywordEntity>>
+
+    @Insert
+    suspend fun insertKeyword(keyword: RecentlySearchKeywordEntity)
+
+    @Query("DELETE FROM recent_search_keyword_table where id = :id ")
+    suspend fun deleteKeyword(id: Long)
+
+    @Query("DELETE FROM recent_search_keyword_table")
+    suspend fun deleteAllKeyword()
+}
