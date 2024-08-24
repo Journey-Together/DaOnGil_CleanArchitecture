051d1a142103994e671eb31a1423921c0b27af3a
chanho0908 <uni070@naver.com>
2024-08-24 16:36:29 +0900

    [Feat] #35 힌트가 있을 때 contentDscription을 중복을 동시에 읽는 문제 해결 확장함수

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/ext/TextViewExt.kt
@@ -0,0 +1,17 @@
+package kr.tekit.lion.presentation.ext
+
+import android.view.View
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.TextView
+
+fun TextView.setAccessibilityText(newText: CharSequence) {
+    accessibilityDelegate = object : View.AccessibilityDelegate() {
+        override fun onInitializeAccessibilityNodeInfo(host: View, info: AccessibilityNodeInfo) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+            if (host is TextView) {
+                info.hintText = null
+                info.text = newText
+            }
+        }
+    }
+}
