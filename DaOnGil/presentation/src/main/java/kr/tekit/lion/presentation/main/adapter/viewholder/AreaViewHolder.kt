11799659882ecb58db6bba162f3192d10cdbad3d
chanho0908 <uni070@naver.com>
2024-08-29 00:15:18 +0900

    [Feat] #39 지역,시군구 선택시 TallBack 메시지 추가

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/viewholder/AreaViewHolder.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/viewholder/AreaViewHolder.kt
@@ -1,9 +1,14 @@
 package kr.tekit.lion.presentation.main.adapter.viewholder
 
+import android.annotation.SuppressLint
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import kr.tekit.lion.presentation.databinding.ItemListSearchAreaBinding
+import kr.tekit.lion.presentation.ext.announceForAccessibility
+import kr.tekit.lion.presentation.ext.isTallBackEnabled
 import kr.tekit.lion.presentation.main.model.AreaModel
 
 class AreaViewHolder(
@@ -25,6 +30,14 @@ class AreaViewHolder(
                 item.areas
             )
             selectedArea.setAdapter(areaAdapter)
+            if (root.context.isTallBackEnabled()) {
+                selectedArea.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+                    val selectedItem = parent.getItemAtPosition(position).toString()
+                    val selectedAreaMessage = "선택된 지역은 ${selectedItem}입니다."
+                    selectedArea.context.announceForAccessibility(selectedAreaMessage)
+                    selectedArea.context.announceForAccessibility("지역선택하단 시군구목록에서 시군구를 선택해보세요")
+                }
+            }
         }
     }
 }
