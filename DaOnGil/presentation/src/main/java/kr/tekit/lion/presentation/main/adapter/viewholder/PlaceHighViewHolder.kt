1ef0ed8ed1e17d00e9e440e35687691d062f5c0d
chanho0908 <uni070@naver.com>
2024-08-29 00:16:15 +0900

    [Feat] #39 관광지 선택시 TallBack 메시지 추가

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/viewholder/PlaceHighViewHolder.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/main/adapter/viewholder/PlaceHighViewHolder.kt
@@ -1,5 +1,7 @@
 package kr.tekit.lion.presentation.main.adapter.viewholder
 
+import android.view.View
+import android.view.accessibility.AccessibilityNodeInfo
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -16,19 +18,40 @@ class PlaceHighViewHolder(private val binding: ItemPlaceHighBinding)
             tvAddr.text = item.placeAddr
             val disabilityList = item.disability
 
-            Glide.with(binding.root)
+            Glide.with(root)
                 .load(item.placeImg)
                 .placeholder(R.drawable.empty_view_small)
                 .error(R.drawable.empty_view_small)
                 .into(thumbnailImg)
 
             val disabilityRVAdapter = DisabilityRVAdapter(disabilityList)
-            binding.touristHighDisabilityRv.adapter = disabilityRVAdapter
-            binding.touristHighDisabilityRv.layoutManager = LinearLayoutManager(
+            touristHighDisabilityRv.adapter = disabilityRVAdapter
+            touristHighDisabilityRv.layoutManager = LinearLayoutManager(
                 root.context,
                 LinearLayoutManager.HORIZONTAL,
                 false
             )
+
+            root.accessibilityDelegate = object : View.AccessibilityDelegate() {
+                override fun onInitializeAccessibilityNodeInfo(host: View, info: AccessibilityNodeInfo) {
+                    super.onInitializeAccessibilityNodeInfo(host, info)
+
+                    val disabilityText = disabilityList.map {
+                        when (it) {
+                            "1" -> root.context.getString(R.string.text_physical_disability)
+                            "2" -> root.context.getString(R.string.text_visual_impairment)
+                            "3" -> root.context.getString(R.string.text_hearing_impairment)
+                            "4" -> root.context.getString(R.string.text_infant_family)
+                            else -> root.context.getString(R.string.text_elderly_person)
+                        }
+                    }
+
+                    val tallBackMsg = "${binding.tvName.text} $disabilityText 에 대한여행을 지원합니다." +
+                            "주소는 ${binding.tvAddr.text}입니다."
+                    info.hintText = null
+                    info.text = tallBackMsg
+                }
+            }
         }
     }
 }
