3f8d63c92c29df971dc6df0dbe6fd033e7f402b7
rmadmsjm <dmswjdals12@naver.com>
2024-08-28 14:18:08 +0900

    [Chore] #36 패키지 변경

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/concerntype/fragment/ConcernTypeModifyFragment.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/concerntype/fragment/ConcernTypeModifyFragment.kt
@@ -1,10 +1,8 @@
-package kr.tekit.lion.presentation.concerntype
+package kr.tekit.lion.presentation.concerntype.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
