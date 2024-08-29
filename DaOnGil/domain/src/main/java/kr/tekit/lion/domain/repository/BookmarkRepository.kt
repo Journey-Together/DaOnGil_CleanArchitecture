f68fd98854214c450a45c955eb1c82a874572fbc
rmadmsjm <dmswjdals12@naver.com>
2024-08-30 00:11:00 +0900

    [Feat] #41 BookmarkRepository 추가

---
+++ DaOnGil/domain/src/main/java/kr/tekit/lion/domain/repository/BookmarkRepository.kt
@@ -0,0 +1,18 @@
+package kr.tekit.lion.domain.repository
+
+import kr.tekit.lion.domain.exception.Result
+import kr.tekit.lion.domain.model.BookmarkedPlace
+import kr.tekit.lion.domain.model.PlaceBookmark
+import kr.tekit.lion.domain.model.PlanBookmark
+
+interface BookmarkRepository {
+    suspend fun getPlaceBookmarkList(): Result<List<BookmarkedPlace>>
+
+    suspend fun getPlaceBookmark(): Result<List<PlaceBookmark>>
+
+    suspend fun getPlanBookmark(): Result<List<PlanBookmark>>
+
+    suspend fun updatePlaceBookmark(placeId: Long): Result<Unit>
+
+    suspend fun updatePlanBookmark(planId: Long): Result<Unit>
+}
