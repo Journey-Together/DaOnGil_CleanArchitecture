e9bdcc769da1f1ac2c0cc31d36ffd3bf2f2d3d73
rmadmsjm <dmswjdals12@naver.com>
2024-08-29 23:59:16 +0900

    [Feat] #41 BookmarkService 추가

---
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/service/BookmarkService.kt
@@ -0,0 +1,29 @@
+package kr.tekit.lion.data.service
+
+import kr.tekit.lion.daongil.data.dto.remote.response.bookmark.PlaceBookmarkListResponse
+import kr.tekit.lion.daongil.data.dto.remote.response.bookmark.PlaceBookmarkResponse
+import kr.tekit.lion.daongil.data.dto.remote.response.bookmark.PlanBookmarkResponse
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
+
+internal interface BookmarkService {
+    @GET("bookmark/names")
+    suspend fun getPlaceBookmarkList() : PlaceBookmarkListResponse
+
+    @GET("bookmark/place")
+    suspend fun getPlaceBookmark() : PlaceBookmarkResponse
+
+    @GET("bookmark/plan")
+    suspend fun getPlanBookmark() : PlanBookmarkResponse
+
+    @PATCH("bookmark/place/{placeId}")
+    suspend fun updatePlaceBookmark(
+        @Path("placeId") placeId: Long
+    )
+
+    @PATCH("bookmark/plan/{planId}")
+    suspend fun updatePlanBookmark(
+        @Path("planId") planId: Long
+    )
+}
