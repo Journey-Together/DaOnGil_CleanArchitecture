108b3ca910e7042ef6cdcd2ba48bb36a307b7139
Boram <dev.lboram@gmail.com>
2024-09-02 02:56:44 +0900

    [Feat] #47 내 일정 목록 Activity 추가

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/myschedule/MyScheduleActivity.kt
@@ -0,0 +1,11 @@
+package kr.tekit.lion.presentation.myschedule
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
