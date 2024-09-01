a6a72f6952706485be71fc3c18242efd63aae82e
yein <inwon9892@naver.com>
2024-09-02 00:16:51 +0900

    [Feat] #57 Aed dto, model 추가

---
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/dto/response/aed/Body.kt
@@ -0,0 +1,11 @@
+package kr.tekit.lion.data.dto.response.aed
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Body(
+    val items: Items,
+    val numOfRows: Int,
+    val pageNo: Int,
+    val totalCount: Int
+)
