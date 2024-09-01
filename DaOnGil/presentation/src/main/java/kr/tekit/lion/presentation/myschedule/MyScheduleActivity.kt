36f12fc64630a332ae6e3abbc70e772cab0457d4
Boram <dev.lboram@gmail.com>
2024-09-02 03:44:10 +0900

    [Design] #59 내 일정 목록 UI

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/myschedule/MyScheduleActivity.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/myschedule/MyScheduleActivity.kt
@@ -2,10 +2,17 @@ package kr.tekit.lion.presentation.myschedule
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kr.tekit.lion.presentation.databinding.ActivityMyScheduleBinding
 
 class MyScheduleActivity : AppCompatActivity() {
+    private val binding: ActivityMyScheduleBinding by lazy {
+        ActivityMyScheduleBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
 
     }
 }
