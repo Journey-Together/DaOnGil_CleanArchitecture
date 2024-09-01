bd93b42968ec884a69c0f760be66ec2a030181fc
yein <inwon9892@naver.com>
2024-09-01 13:51:01 +0900

    [Feat] #51 EmergencyMapActivity의 viewmodel 추가

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/emergency/EmergencyMapActivity.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/emergency/EmergencyMapActivity.kt
@@ -2,11 +2,23 @@ package kr.tekit.lion.presentation.emergency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kr.tekit.lion.presentation.R
+import kr.tekit.lion.presentation.databinding.ActivityEmergencyMapBinding
+import kr.tekit.lion.presentation.emergency.vm.EmergencyMapViewModel
 
+@AndroidEntryPoint
 class EmergencyMapActivity : AppCompatActivity() {
+
+    private val binding: ActivityEmergencyMapBinding by lazy {
+        ActivityEmergencyMapBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel: EmergencyMapViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_emergency_map)
+        setContentView(binding.root)
     }
 }
