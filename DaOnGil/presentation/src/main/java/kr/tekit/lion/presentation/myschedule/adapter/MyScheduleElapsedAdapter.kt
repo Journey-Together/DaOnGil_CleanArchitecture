84cc92867ce4dc7c7d967dec5d57429467e89a93
Boram <dev.lboram@gmail.com>
2024-09-02 14:01:00 +0900

    [Feat] #59 다가오는 일정목록, 다녀온 일정 목록 Adapter

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/myschedule/adapter/MyScheduleElapsedAdapter.kt
@@ -0,0 +1,95 @@
+package kr.tekit.lion.presentation.myschedule.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kr.tekit.lion.domain.model.schedule.MyElapsedScheduleInfo
+import kr.tekit.lion.presentation.R
+import kr.tekit.lion.presentation.databinding.ItemMyScheduleElapsedBinding
+
+class MyScheduleElapsedAdapter(
+    private val onReviewButtonClicked: (planPosition: Int) -> Unit,
+    private val onScheduleItemClicked: (planPosition: Int) -> Unit
+) : ListAdapter<MyElapsedScheduleInfo, MyScheduleElapsedAdapter.ElapsedScheduleViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElapsedScheduleViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ElapsedScheduleViewHolder(
+            ItemMyScheduleElapsedBinding.inflate(
+                inflater,
+                parent,
+                false
+            ), onReviewButtonClicked, onScheduleItemClicked
+        )
+    }
+
+    override fun onBindViewHolder(holder: ElapsedScheduleViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ElapsedScheduleViewHolder(
+        private val binding: ItemMyScheduleElapsedBinding,
+        private val onReviewButtonClicked: (Int) -> Unit,
+        private val onScheduleItemClicked: (Int) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.buttonMyScheduleElapsedReview.setOnClickListener {
+                onReviewButtonClicked(absoluteAdapterPosition)
+            }
+            binding.root.setOnClickListener {
+                onScheduleItemClicked(absoluteAdapterPosition)
+            }
+        }
+
+        fun bind(mySchedule: MyElapsedScheduleInfo) {
+            binding.apply {
+                buttonMyScheduleElapsedReview.apply {
+                    if (mySchedule.hasReview) {
+                        visibility = View.GONE
+                    } else {
+                        visibility = View.VISIBLE
+                    }
+                }
+
+                if (mySchedule.imageUrl != "") {
+                    Glide.with(itemView.context)
+                        .load(mySchedule.imageUrl)
+                        .placeholder(R.drawable.empty_view_small)
+                        .error(R.drawable.empty_view_small)
+                        .override(50, 50)
+                        .into(binding.imageViewMyScheduleElapsed)
+                }
+
+                textViewMyScheduleElapsedName.text = mySchedule.title
+                textViewMyScheduleElapsedPeriod.text = itemView.context.getString(
+                    R.string.text_schedule_period,
+                    mySchedule.startDate,
+                    mySchedule.endDate
+                )
+            }
+        }
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<MyElapsedScheduleInfo>() {
+            override fun areItemsTheSame(
+                oldItem: MyElapsedScheduleInfo,
+                newItem: MyElapsedScheduleInfo
+            ): Boolean {
+                return oldItem.planId == newItem.planId
+            }
+
+            override fun areContentsTheSame(
+                oldItem: MyElapsedScheduleInfo,
+                newItem: MyElapsedScheduleInfo
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
