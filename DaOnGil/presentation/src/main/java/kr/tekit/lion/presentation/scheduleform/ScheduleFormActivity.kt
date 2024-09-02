87aeb0f84aa8078bb1d027687f6aa88e510aaeb2
Boram <dev.lboram@gmail.com>
2024-09-02 17:55:01 +0900

    [Design] #62 여행 일정 추가 Activity UI

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/scheduleform/ScheduleFormActivity.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/scheduleform/ScheduleFormActivity.kt
@@ -2,10 +2,16 @@ package kr.tekit.lion.presentation.scheduleform
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kr.tekit.lion.presentation.databinding.ActivityScheduleFormBinding
 
 class ScheduleFormActivity : AppCompatActivity() {
+
+    private val binding: ActivityScheduleFormBinding by lazy {
+        ActivityScheduleFormBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContentView(binding.root)
     }
 }
