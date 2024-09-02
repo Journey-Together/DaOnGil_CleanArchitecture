d9e8cb195cb0f206aec28229fd2b4518db120313
Boram <dev.lboram@gmail.com>
2024-09-02 16:45:22 +0900

    [Feat] #62 여행 일정 추가 화면 생성

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/scheduleform/ScheduleFormActivity.kt
@@ -0,0 +1,11 @@
+package kr.tekit.lion.presentation.scheduleform
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ScheduleFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
