73f58fddb063afaf7a7132269b1a51eb2b1d7a64
chanho0908 <uni070@naver.com>
2024-09-03 17:40:22 +0900

    [Feat] #55 키워드 검색 메인 뷰모델

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/keyword/vm/KeywordSearchViewModel.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/keyword/vm/KeywordSearchViewModel.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOn
@@ -24,8 +25,10 @@ import kr.tekit.lion.domain.model.search.toRecentlySearchKeyword
 import kr.tekit.lion.domain.repository.PlaceRepository
 import kr.tekit.lion.domain.repository.RecentlySearchKeywordRepository
 import kr.tekit.lion.presentation.delegate.NetworkErrorDelegate
+import kr.tekit.lion.presentation.keyword.model.KeywordInputState
 import kr.tekit.lion.presentation.keyword.model.KeywordSearch
 import kr.tekit.lion.presentation.main.model.PlaceModel
+import kr.tekit.lion.presentation.main.model.toUiModel
 import javax.inject.Inject
 
 @HiltViewModel
@@ -45,8 +48,8 @@ class KeywordSearchViewModel @Inject constructor(
 
     val errorMessage: StateFlow<String?> get() = networkErrorDelegate.errorMessage
 
-    private val _place: MutableStateFlow<PlaceModel> = MutableStateFlow(PlaceModel())
-    val place: StateFlow<PlaceModel> = _place.asStateFlow()
+    private val _place: MutableStateFlow<List<PlaceModel>> = MutableStateFlow(emptyList())
+    val place: StateFlow<List<PlaceModel>> = _place.asStateFlow()
 
     private val _recentlySearchKeyword = MutableStateFlow<List<RecentlySearchKeyword>>(emptyList())
     val recentlySearchKeyword = _recentlySearchKeyword.asStateFlow()
@@ -54,10 +57,14 @@ class KeywordSearchViewModel @Inject constructor(
     private val _keyword = MutableStateFlow("")
     val keyword = _keyword.asStateFlow()
 
-    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    private val _searchState = MutableStateFlow(KeywordInputState.Initial)
+    val searchState = _searchState.asStateFlow()
+
+    @OptIn(FlowPreview:: class,  ExperimentalCoroutinesApi:: class)
     val autocompleteKeyword = keyword
-        .filter { it.isNotBlank() }
         .debounce(DEBOUNCE_INTERVAL)
+        .distinctUntilChanged()
+        .filter { it.isNotEmpty() }
         .flatMapLatest { keyword ->
             placeRepository.getAutoCompleteKeyword(keyword)
         }
@@ -68,15 +75,22 @@ class KeywordSearchViewModel @Inject constructor(
             }
         }
 
-    fun updateKeyword(keyword: String) {
-        _keyword.value = keyword
+
+    fun inputTextChanged(keyword: String) {
+        if (searchState.value ==  KeywordInputState.NotEmpty) {
+            _keyword.update { keyword }
+        }
+    }
+
+    fun keywordInputStateChanged(state: KeywordInputState) {
+        _searchState.value = state
     }
 
     fun onClickSearchButton(keyword: String) = viewModelScope.launch(Dispatchers.IO){
         placeRepository.getSearchPlaceResultByList(
             KeywordSearch(keyword = keyword, page = 0).toDomainModel()
         ).onSuccess {
-            _place.update { it }
+            _place.value = it.toUiModel()
         }.onError {
             networkErrorDelegate.handleNetworkError(it)
         }
@@ -88,19 +102,20 @@ class KeywordSearchViewModel @Inject constructor(
         }
     }
 
-    fun insertKeyword(keyword: String) = viewModelScope.launch(Dispatchers.IO) {
+    fun insertKeyword(keyword: String, onSuccess: () -> Unit) = viewModelScope.launch(Dispatchers.IO) {
         val existingKeyword = _recentlySearchKeyword.value.firstOrNull { it.keyword == keyword }
         if (existingKeyword != null) {
             existingKeyword.id?.let { recentlySearchKeywordRepository.deleteKeyword(it) }
         }
         recentlySearchKeywordRepository.insertKeyword(keyword.toRecentlySearchKeyword())
+        onSuccess.invoke()
     }
 
     fun deleteKeyword(id: Long) = viewModelScope.launch(Dispatchers.IO) {
         recentlySearchKeywordRepository.deleteKeyword(id)
     }
 
-    fun deleteAllKeyword() = viewModelScope.launch(Dispatchers.IO) {
+    fun onClickDeleteButton() = viewModelScope.launch(Dispatchers.IO) {
         recentlySearchKeywordRepository.deleteAllKeyword()
     }
 
