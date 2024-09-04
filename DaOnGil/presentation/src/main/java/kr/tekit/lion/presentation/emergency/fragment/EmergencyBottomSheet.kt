12f0495d5d5b064ccc6a66c5276aaeb9d035f449
yein <inwon9892@naver.com>
2024-09-05 02:11:00 +0900

    [Feat] #66 약국 지도에서 사용할 버텀시트 추가

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/emergency/fragment/EmergencyBottomSheet.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/emergency/fragment/EmergencyBottomSheet.kt
@@ -6,7 +6,7 @@ import kr.tekit.lion.domain.model.EmergencyMapInfo
 import kr.tekit.lion.presentation.databinding.EmergencyBottomSheetLayoutBinding
 import kr.tekit.lion.presentation.emergency.EmergencyInfoActivity
 import kr.tekit.lion.presentation.emergency.adapter.EmergencyBottomAdapter
-import kr.tekit.lion.presentation.model.toEmergencyMap
+import kr.tekit.lion.presentation.model.toEmergencyInfo
 
 class EmergencyBottomSheet(
     private val binding: EmergencyBottomSheetLayoutBinding,
@@ -19,7 +19,7 @@ class EmergencyBottomSheet(
                 val context: Context = binding.root.context
                 val intent = Intent(context, EmergencyInfoActivity::class.java)
                 val emergencyInfoList = emergencyMapInfoList.map {
-                    it.toEmergencyMap()
+                    it.toEmergencyInfo()
                 }
                 intent.putExtra("infoType", emergencyMapInfoList[position].emergencyType)
                 intent.putExtra("data",emergencyInfoList[position])
