1a4fdada452504abfe86cd09dfae352bd4a5ed3b
rmadmsjm <dmswjdals12@naver.com>
2024-09-05 17:47:37 +0900

    [Feat] #48 나의 여행지 후기 dto response data class 추가
    MyPlaceReviewData, MyPlaceReviewDto, MyPlaceReviewResponse

---
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/dto/response/myreview/MyPlaceReviewDto.kt
@@ -0,0 +1,15 @@
+package kr.tekit.lion.data.dto.response.myreview
+
+import com.squareup.moshi.JsonClass
+import java.time.LocalDate
+
+@JsonClass(generateAdapter = true)
+internal data class MyPlaceReviewDto(
+    val content: String,
+    val date: LocalDate,
+    val grade: Float,
+    val images: List<String>,
+    val name: String,
+    val placeId: Long,
+    val reviewId: Long
+)
