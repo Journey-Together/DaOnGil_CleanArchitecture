e8f8bdd43fa372672a41bceaf97d35bc8f2d48ea
yein <inwon9892@naver.com>
2024-09-06 02:03:55 +0900

    [Feat] #76 일정 상세보기 화면에서 사용할 usecase 추가 (일정 상세보기, 리뷰 상세보기)

---
+++ DaOnGil/domain/src/main/java/kr/tekit/lion/domain/usecase/plan/GetScheduleDetailUseCase.kt
@@ -0,0 +1,46 @@
+package kr.tekit.lion.domain.usecase.plan
+
+import kr.tekit.lion.domain.model.ScheduleDetail
+import kr.tekit.lion.domain.model.ScheduleDetailInfo
+import kr.tekit.lion.domain.model.ScheduleDetailReview
+import kr.tekit.lion.domain.repository.PlanRepository
+import kr.tekit.lion.domain.usecase.base.BaseUseCase
+import kr.tekit.lion.domain.usecase.base.Result
+import javax.inject.Inject
+
+class GetScheduleDetailUseCase @Inject constructor(
+    private val planRepository: PlanRepository
+): BaseUseCase() {
+    suspend operator fun invoke(planId: Long): Result<ScheduleDetail> = execute {
+
+        val scheduleInfo = planRepository.getDetailScheduleInfo(planId)
+        val reviewInfo = planRepository.getDetailScheduleReview(planId)
+
+        combineScheduleDetail(scheduleInfo, reviewInfo)
+    }
+}
+
+
+private fun combineScheduleDetail(
+    info: ScheduleDetailInfo,
+    review: ScheduleDetailReview
+): ScheduleDetail {
+    return ScheduleDetail(
+        title = info.title,
+        startDate = info.startDate,
+        endDate = info.endDate,
+        remainDate = info.remainDate,
+        isPublic = info.isPublic,
+        isWriter = info.isWriter,
+        nickname = info.nickname,
+        images = info.images,
+        dailyPlans = info.dailyPlans,
+        writerId = info.writerId,
+        reviewId = review.reviewId,
+        content = review.content,
+        grade = review.grade,
+        reviewImages = review.imageList,
+        hasReview = review.hasReview,
+        profileUrl = review.profileUrl
+    )
+}
