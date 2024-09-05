49f7bfe04a6814de7ea21d3704df3920a73b0219
rmadmsjm <dmswjdals12@naver.com>
2024-09-05 17:56:54 +0900

    [Feat] #48 나의 여행지 후기 이미지 RecyclerView Adapter 추가

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/myreview/adapter/MyReviewImageRVAdapter.kt
@@ -0,0 +1,53 @@
+package kr.tekit.lion.presentation.myreview.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import dagger.hilt.android.internal.managers.ViewComponentManager
+import kr.tekit.lion.presentation.R
+import kr.tekit.lion.presentation.databinding.ItemReviewBigImageBinding
+import kr.tekit.lion.presentation.ext.showPhotoDialog
+
+class MyReviewImageRVAdapter(private val imageList: List<String>) :
+    RecyclerView.Adapter<MyReviewImageRVAdapter.MyReviewImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyReviewImageViewHolder {
+        val binding = ItemReviewBigImageBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+
+        return MyReviewImageViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = imageList.size
+
+    override fun onBindViewHolder(holder: MyReviewImageViewHolder, position: Int) {
+        holder.bind(imageList[position], imageList, position)
+    }
+
+    class MyReviewImageViewHolder(
+        private val binding: ItemReviewBigImageBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(image: String, imageList: List<String>, position: Int) {
+            Glide.with(binding.reviewImage.context)
+                .load(image)
+                .error(R.drawable.empty_view_small)
+                .into(binding.reviewImage)
+
+            binding.reviewImage.setOnClickListener {
+                val context = binding.root.context
+
+                // FragmentContextWrapper에서 FragmentActivity 추출
+                val activityContext = (context as? ViewComponentManager.FragmentContextWrapper)?.baseContext as? FragmentActivity
+
+                if (activityContext != null) {
+                    val fragmentManager = activityContext.supportFragmentManager
+                    activityContext.showPhotoDialog(fragmentManager, imageList, position)
+                }
+            }
+        }
+    }
+}
