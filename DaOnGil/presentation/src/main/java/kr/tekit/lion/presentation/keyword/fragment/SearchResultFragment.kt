49d39c04b56f24e9519eb93cc8b7a36664149060
chanho0908 <uni070@naver.com>
2024-09-06 20:42:06 +0900

    [Feat] #55 데이터 로딩시 프로그래스바 구현 완료

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/keyword/fragment/SearchResultFragment.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/keyword/fragment/SearchResultFragment.kt
@@ -2,15 +2,19 @@ package kr.tekit.lion.presentation.keyword.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import kr.tekit.lion.presentation.R
 import kr.tekit.lion.presentation.databinding.FragmentSearchResultBinding
+import kr.tekit.lion.presentation.delegate.NetworkState
 import kr.tekit.lion.presentation.ext.addOnScrollEndListener
 import kr.tekit.lion.presentation.ext.hideSoftInput
 import kr.tekit.lion.presentation.ext.repeatOnViewStarted
@@ -37,7 +41,7 @@ class SearchResultFragment : Fragment(R.layout.fragment_search_result) {
         }
 
 
-        val rvAdapter = SearchResultAdapter{
+        val rvAdapter = SearchResultAdapter {
 
         }
 
@@ -53,16 +57,42 @@ class SearchResultFragment : Fragment(R.layout.fragment_search_result) {
         }
 
         repeatOnViewStarted {
-            viewModel.place.collect{
-                if (it.isEmpty()){
-                    binding.noSearchResultContainer.visibility = View.VISIBLE
-                    binding.searchResultRv.visibility = View.GONE
-                }else{
-                    binding.noSearchResultContainer.visibility = View.GONE
-                    binding.searchResultRv.visibility = View.VISIBLE
-                    rvAdapter.submitList(it)
+            viewModel.networkState.collectLatest { state ->
+                val progressBar =
+                    requireActivity().findViewById<ProgressBar>(R.id.search_view_progressBar)
+                when (state) {
+                    is NetworkState.Loading -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+
+                    is NetworkState.Success -> {
+                        progressBar.visibility = View.GONE
+                    }
                 }
             }
+        }
+
+        repeatOnViewStarted {
+            combine(viewModel.networkState, viewModel.place) { networkState, place ->
+                val progressBar = requireActivity().findViewById<ProgressBar>(R.id.search_view_progressBar)
+                when (networkState) {
+                    is NetworkState.Loading -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+
+                    is NetworkState.Success -> {
+                        progressBar.visibility = View.GONE
+                        if (place.isEmpty()) {
+                            binding.noSearchResultContainer.visibility = View.VISIBLE
+                            binding.searchResultRv.visibility = View.GONE
+                        } else {
+                            binding.noSearchResultContainer.visibility = View.GONE
+                            binding.searchResultRv.visibility = View.VISIBLE
+                            rvAdapter.submitList(place)
+                        }
+                    }
+                }
+            }.collect { }
         }
     }
 }
