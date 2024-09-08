f9f7e2c6680abab375a263743c7d160cf53a80f1
Jieun <lje2000lje@naver.com>
2024-09-08 13:50:04 +0900

    [Feat] #109 리뷰 전체보기 뷰모델 게스트ver 함수 추가

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/vm/ReviewListViewModel.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/vm/ReviewListViewModel.kt
@@ -1,26 +1,33 @@
 package kr.tekit.lion.presentation.home.vm
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kr.tekit.lion.domain.exception.onError
 import kr.tekit.lion.domain.exception.onSuccess
 import kr.tekit.lion.domain.model.placereviewlist.PlaceReviewInfo
+import kr.tekit.lion.domain.repository.AuthRepository
 import kr.tekit.lion.domain.repository.PlaceRepository
 import kr.tekit.lion.presentation.delegate.NetworkErrorDelegate
+import kr.tekit.lion.presentation.splash.model.LogInState
 import javax.inject.Inject
 
 @HiltViewModel
 class ReviewListViewModel @Inject constructor(
+    private val authRepository: AuthRepository,
     private val getPlaceReviewListRepository: PlaceRepository
 ) : ViewModel() {
 
     @Inject
     lateinit var networkErrorDelegate: NetworkErrorDelegate
+
+    private val _loginState = MutableStateFlow<LogInState>(LogInState.Checking)
+    val loginState = _loginState.asStateFlow()
 
     private val _placeReviewInfo = MutableLiveData<PlaceReviewInfo>()
     val placeReviewInfo : LiveData<PlaceReviewInfo> = _placeReviewInfo
@@ -34,10 +41,29 @@ class ReviewListViewModel @Inject constructor(
 
     init {
         _isLastPage.value = false
+
+        viewModelScope.launch {
+            checkLoginState()
+        }
+    }
+
+    private suspend fun checkLoginState(){
+        authRepository.loggedIn.collect{ isLoggedIn ->
+            if (isLoggedIn) _loginState.value = LogInState.LoggedIn
+            else _loginState.value = LogInState.LoginRequired
+        }
     }
 
     fun getPlaceReview(placeId: Long) = viewModelScope.launch {
         getPlaceReviewListRepository.getPlaceReviewList(placeId, PAGE_SIZE, 0).onSuccess {
+            _placeReviewInfo.value = it
+        }.onError {
+            networkErrorDelegate.handleNetworkError(it)
+        }
+    }
+
+    fun getPlaceReviewGuest(placeId: Long) = viewModelScope.launch {
+        getPlaceReviewListRepository.getPlaceReviewListGuest(placeId, PAGE_SIZE, 0).onSuccess {
             _placeReviewInfo.value = it
         }.onError {
             networkErrorDelegate.handleNetworkError(it)
@@ -49,6 +75,26 @@ class ReviewListViewModel @Inject constructor(
 
         if (page != null) {
             getPlaceReviewListRepository.getPlaceReviewList(placeId, size = PAGE_SIZE, page = page + 1).onSuccess {
+                if (it.pageNo == it.totalPages) {
+                    _isLastPage.value = true
+                } else {
+                    val reviews = _placeReviewInfo.value?.placeReviewList ?: emptyList()
+                    val newReviews = reviews + it.placeReviewList
+                    val newReviewData = it.copy(placeReviewList = newReviews)
+
+                    _placeReviewInfo.value = newReviewData
+                }
+            }.onError {
+                networkErrorDelegate.handleNetworkError(it)
+            }
+        }
+    }
+
+    fun getNewPlaceReviewGuest(placeId: Long) = viewModelScope.launch {
+        val page = _placeReviewInfo.value?.pageNo
+
+        if (page != null) {
+            getPlaceReviewListRepository.getPlaceReviewListGuest(placeId, size = PAGE_SIZE, page = page + 1).onSuccess {
                 if (it.pageNo == it.totalPages) {
                     _isLastPage.value = true
                 } else {
