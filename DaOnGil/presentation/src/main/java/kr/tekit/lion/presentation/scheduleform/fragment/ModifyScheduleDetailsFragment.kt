6bd1294780aa41cdf98813ad43cdb20d2c38c1c1
Boram <dev.lboram@gmail.com>
2024-09-08 17:13:16 +0900

    [Design] #112 일정 수정 fragment 생성

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/scheduleform/fragment/ModifyScheduleDetailsFragment.kt
@@ -0,0 +1,16 @@
+package kr.tekit.lion.presentation.scheduleform.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import kr.tekit.lion.presentation.R
+import kr.tekit.lion.presentation.databinding.FragmentModifyScheduleDetailsBinding
+
+class ModifyScheduleDetailsFragment : Fragment(R.layout.fragment_modify_schedule_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentModifyScheduleDetailsBinding.bind(view)
+    }
+}
