5e34532b18432d7d202c0b096893c50cac7fdf51
Boram <dev.lboram@gmail.com>
2024-09-08 17:41:47 +0900

    [Feat] #112 일정 수정 navigation graph

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/scheduleform/fragment/ModifySearchFragment.kt
@@ -0,0 +1,17 @@
+package kr.tekit.lion.presentation.scheduleform.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import kr.tekit.lion.presentation.R
+import kr.tekit.lion.presentation.databinding.FragmentFormSearchBinding
+
+class ModifySearchFragment : Fragment(R.layout.fragment_form_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentFormSearchBinding.bind(view)
+    }
+
+}
