fa4e4dea46d7f9b3a0f1b66bc89bd511c7dcc2ff
Boram <dev.lboram@gmail.com>
2024-09-08 22:17:50 +0900

    [Feat] #112 여행 제목, 기간 수정 화면 기능 구현

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/scheduleform/fragment/ModifyNamePeriodFragment.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/scheduleform/fragment/ModifyNamePeriodFragment.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.MaterialDatePicker
 import dagger.hilt.android.AndroidEntryPoint
 import kr.tekit.lion.presentation.R
 import kr.tekit.lion.presentation.databinding.FragmentModifyNamePeriodBinding
+import kr.tekit.lion.presentation.ext.showSnackbar
 import kr.tekit.lion.presentation.scheduleform.model.OriginalScheduleInfo
 import kr.tekit.lion.presentation.scheduleform.vm.ModifyScheduleFormViewModel
+import java.util.Date
 
 @AndroidEntryPoint
 class ModifyNamePeriodFragment : Fragment(R.layout.fragment_modify_name_period) {
@@ -21,13 +25,24 @@ class ModifyNamePeriodFragment : Fragment(R.layout.fragment_modify_name_period) 
 
         val binding = FragmentModifyNamePeriodBinding.bind(view)
 
+        initToolbar(binding)
         initScheduleData(binding)
+        initButtonClickListener(binding)
 
     }
 
-    private fun initScheduleData(binding:FragmentModifyNamePeriodBinding){
-        val originalScheduleInfo = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            requireActivity().intent.getParcelableExtra("scheduleInfo", OriginalScheduleInfo::class.java)
+    private fun initToolbar(binding: FragmentModifyNamePeriodBinding){
+        binding.toolbarModifyNpf.setNavigationOnClickListener {
+            requireActivity().finish()
+        }
+    }
+
+    private fun initScheduleData(binding: FragmentModifyNamePeriodBinding) {
+        val originalScheduleInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requireActivity().intent.getParcelableExtra(
+                "scheduleInfo",
+                OriginalScheduleInfo::class.java
+            )
         } else {
             requireActivity().intent.getParcelableExtra("scheduleInfo") as? OriginalScheduleInfo
         }
@@ -36,17 +51,68 @@ class ModifyNamePeriodFragment : Fragment(R.layout.fragment_modify_name_period) 
             viewModel.initScheduleDetailInfo(it)
         }
 
-        initScehduleTitleAndPeriod(binding)
+        initScheduleTitleAndPeriod(binding)
     }
 
-    private fun initScehduleTitleAndPeriod(binding:FragmentModifyNamePeriodBinding){
-        viewModel.title.observe(viewLifecycleOwner){
+    private fun initScheduleTitleAndPeriod(binding: FragmentModifyNamePeriodBinding) {
+        viewModel.title.observe(viewLifecycleOwner) {
             binding.editModiyNpfTitle.setText(it)
         }
-        viewModel.endDate.observe(viewLifecycleOwner){
+        viewModel.endDate.observe(viewLifecycleOwner) {
             val pickedDates = viewModel.formatPickedDates()
             binding.buttonModifyNpfSetPeriod.text = pickedDates
         }
-
     }
+
+    private fun initButtonClickListener(binding: FragmentModifyNamePeriodBinding) {
+        with(binding) {
+            buttonModifyNpfSetPeriod.setOnClickListener {
+                val dateRangePicker = MaterialDatePicker.Builder.dateRangePicker()
+                    .setTheme(R.style.DateRangePickerTheme)
+                    .setTitleText("조회기간을 설정해주세요")
+                    .build()
+
+                dateRangePicker.show(requireActivity().supportFragmentManager, "ModifyScheduleFormSetPeriod")
+                dateRangePicker.addOnPositiveButtonClickListener {
+                    viewModel.setStartDate(Date(it.first))
+                    viewModel.setEndDate(Date(it.second))
+                }
+            }
+
+            buttonModifyNpfNextStep.setOnClickListener { view ->
+                val isNameAndPeriodValidate = validateScheduleNameAndPeriod(this)
+
+                if(isNameAndPeriodValidate){
+                    viewModel.setTitle(editModiyNpfTitle.text.toString())
+
+                    val navController = findNavController()
+                    val action = ModifyNamePeriodFragmentDirections.toModifyScheduleDetailsFragment()
+                    navController.navigate(action)
+                }
+            }
+        }
+    }
+
+    private fun validateScheduleNameAndPeriod(binding: FragmentModifyNamePeriodBinding): Boolean {
+        with(binding) {
+            editModiyNpfTitle.apply {
+                val tempName = this.text.toString()
+
+                if (tempName.isEmpty()) {
+                    textInputModifyNpfTitle.error = "제목은 1글자 이상 입력해주세요"
+                    return false
+                }
+            }
+
+            val hasStartDate = viewModel.hasStartDate()
+
+            if (!hasStartDate) {
+                buttonModifyNpfSetPeriod.showSnackbar("여행 기간을 설정해주세요")
+                return false
+            }
+        }
+
+        return true
+    }
+
 }
