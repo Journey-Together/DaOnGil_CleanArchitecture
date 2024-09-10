70b9ca09d41c1bbd4b8279eabdaec7a4365d9e01
rmadmsjm <dmswjdals12@naver.com>
2024-09-10 22:44:04 +0900

    [Refactor] #122 ReportRepository reportReview 파라미터 변경

--- DaOnGil/domain/src/main/java/kr/tekit/lion/domain/repository/ReportRepository.kt
+++ DaOnGil/domain/src/main/java/kr/tekit/lion/domain/repository/ReportRepository.kt
@@ -4,5 +4,5 @@ import kr.tekit.lion.domain.exception.Result
 import kr.tekit.lion.domain.model.ReportReview
 
 interface ReportRepository {
-    suspend fun reportReview(reviewType: String, reportReviewReq: ReportReview): Result<Unit>
+    suspend fun reportReview(reviewType: String, request: ReportReview): Result<Unit>
 }
