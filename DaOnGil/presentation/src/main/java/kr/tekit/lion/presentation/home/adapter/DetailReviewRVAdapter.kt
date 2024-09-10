825a2b651daea6b4454954be8ab8f0100f25d49b
Jieun <lje2000lje@naver.com>
2024-09-11 02:15:58 +0900

    [Feat] #161 관광지 상세보기 리뷰 신고하기 로그인 유무에 따라 분기처리

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/adapter/DetailReviewRVAdapter.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/adapter/DetailReviewRVAdapter.kt
@@ -4,16 +4,20 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kr.tekit.lion.domain.model.detailplace.Review
 import kr.tekit.lion.presentation.R
 import kr.tekit.lion.presentation.databinding.ItemDetailReviewBigBinding
+import kr.tekit.lion.presentation.login.LoginActivity
+import kr.tekit.lion.presentation.main.dialog.ConfirmDialog
 import kr.tekit.lion.presentation.report.ReportActivity
 
 class DetailReviewRVAdapter(
-    private val reviewList: List<Review>
+    private val reviewList: List<Review>,
+    private val loginState: Boolean
 ) : RecyclerView.Adapter<DetailReviewRVAdapter.DetailReviewViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailReviewViewHolder {
@@ -21,7 +25,7 @@ class DetailReviewRVAdapter(
             LayoutInflater.from(parent.context), parent, false
         )
 
-        return DetailReviewViewHolder(binding)
+        return DetailReviewViewHolder(binding, loginState)
     }
 
     override fun getItemCount(): Int = reviewList.size
@@ -31,7 +35,8 @@ class DetailReviewRVAdapter(
     }
 
     class DetailReviewViewHolder(
-        private val binding: ItemDetailReviewBigBinding
+        private val binding: ItemDetailReviewBigBinding,
+        private val loginState: Boolean
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(reviewData: Review) {
             with(binding) {
@@ -67,15 +72,35 @@ class DetailReviewRVAdapter(
                 }
 
                 itemDetailReviewBigReportBtn.setOnClickListener {
-                    val context = binding.root.context
+                    if (loginState) {
+                        val context = binding.root.context
 
-                    val intent = Intent(context, ReportActivity::class.java).apply {
-                        putExtra("reviewType", "PlaceReview")
-                        putExtra("reviewId", reviewData.reviewId)
+                        val intent = Intent(context, ReportActivity::class.java).apply {
+                            putExtra("reviewType", "PlaceReview")
+                            putExtra("reviewId", reviewData.reviewId)
+                        }
+                        context.startActivity(intent)
+                    } else {
+                        displayLoginDialog("후기를 신고하고 싶다면\n 로그인을 진행해주세요", binding)
                     }
-                    context.startActivity(intent)
                 }
+            }
+        }
 
+        private fun displayLoginDialog(subtitle: String, binding: ItemDetailReviewBigBinding) {
+            val context = binding.root.context
+            val dialog = ConfirmDialog(
+                "로그인이 필요해요!",
+                subtitle,
+                "로그인하기",
+            ) {
+                val intent = Intent(context, LoginActivity::class.java)
+                context.startActivity(intent)
+            }
+
+            if (context is FragmentActivity) {
+                dialog.isCancelable = true
+                dialog.show(context.supportFragmentManager, "ScheduleLoginDialog")
             }
         }
     }
