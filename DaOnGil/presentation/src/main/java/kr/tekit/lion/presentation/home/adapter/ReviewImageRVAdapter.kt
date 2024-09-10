4bc6872bd0a27cd05c08ae498d44d866bcb0a4b5
Jieun <lje2000lje@naver.com>
2024-09-10 16:06:22 +0900

    [Feat] #143 리뷰 이미지 클릭 시 크게 보기 설정

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/adapter/ReviewImageRVAdapter.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/adapter/ReviewImageRVAdapter.kt
@@ -2,12 +2,18 @@ package kr.tekit.lion.presentation.home.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kr.tekit.lion.presentation.R
 import kr.tekit.lion.presentation.databinding.ItemReviewBigImageBinding
+import kr.tekit.lion.presentation.ext.setImageSmall
+import kr.tekit.lion.presentation.ext.showPhotoDialog
 
-class ReviewImageRVAdapter(private val imageList: List<String>) :
+class ReviewImageRVAdapter(
+    private val imageList: List<String>,
+    private val activity: AppCompatActivity
+) :
     RecyclerView.Adapter<ReviewImageRVAdapter.ReviewImageViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewImageViewHolder {
@@ -15,23 +21,28 @@ class ReviewImageRVAdapter(private val imageList: List<String>) :
             LayoutInflater.from(parent.context), parent, false
         )
 
-        return ReviewImageViewHolder(binding)
+        return ReviewImageViewHolder(binding, activity)
     }
 
     override fun getItemCount(): Int = imageList.size
 
     override fun onBindViewHolder(holder: ReviewImageViewHolder, position: Int) {
-        holder.bind(imageList[position])
+        holder.bind(imageList[position], imageList, position)
     }
 
     class ReviewImageViewHolder(
-        val binding: ItemReviewBigImageBinding
+        val binding: ItemReviewBigImageBinding,
+        val activity: AppCompatActivity
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(image: String) {
-            Glide.with(binding.reviewImage.context)
-                .load(image)
-                .error(R.drawable.empty_view_small)
-                .into(binding.reviewImage)
+        fun bind(image: String, imageList: List<String>, position: Int) {
+
+            binding.root.context.setImageSmall(binding.reviewImage, image)
+
+            binding.reviewImage.setOnClickListener {
+                activity.baseContext.showPhotoDialog(
+                    activity.supportFragmentManager, imageList, position
+                )
+            }
         }
     }
 }
