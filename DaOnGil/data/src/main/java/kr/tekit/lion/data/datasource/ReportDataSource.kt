2a834ff9c17607fda51781767c17a2cf87623d68
rmadmsjm <dmswjdals12@naver.com>
2024-09-10 16:43:21 +0900

    [Feat] #122 ReportDataSource 추가

---
+++ DaOnGil/data/src/main/java/kr/tekit/lion/data/datasource/ReportDataSource.kt
@@ -0,0 +1,14 @@
+package kr.tekit.lion.data.datasource
+
+import kr.tekit.lion.data.common.execute
+import kr.tekit.lion.data.service.ReportService
+import okhttp3.RequestBody
+import javax.inject.Inject
+
+internal class ReportDataSource @Inject constructor(
+    private val reportService: ReportService
+) {
+    suspend fun reportReview(reviewType: String, reportReviewReq: RequestBody) = execute {
+        reportService.reportReview(reviewType, reportReviewReq)
+    }
+}
