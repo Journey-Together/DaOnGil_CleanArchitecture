4f147bff929ce25493a59b63e7b7df427ed8efb6
rmadmsjm <dmswjdals12@naver.com>
2024-09-10 16:47:24 +0900

    [Feat] #122 신고하기 기능 구현

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/report/ReportActivity.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/report/ReportActivity.kt
@@ -1,7 +1,9 @@
 package kr.tekit.lion.presentation.report
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
@@ -11,7 +13,10 @@ import com.google.android.material.textfield.TextInputLayout
 import dagger.hilt.android.AndroidEntryPoint
 import kr.tekit.lion.presentation.R
 import kr.tekit.lion.presentation.databinding.ActivityReportBinding
+import kr.tekit.lion.presentation.delegate.NetworkState
+import kr.tekit.lion.presentation.ext.repeatOnStarted
 import kr.tekit.lion.presentation.ext.showSoftInput
+import kr.tekit.lion.presentation.report.vm.ReportViewModel
 
 @AndroidEntryPoint
 class ReportActivity : AppCompatActivity() {
@@ -20,11 +25,27 @@ class ReportActivity : AppCompatActivity() {
         ActivityReportBinding.inflate(layoutInflater)
     }
 
+    private val viewModel: ReportViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        repeatOnStarted {
+            viewModel.networkState.collect { networkState ->
+                when (networkState) {
+                    is NetworkState.Success -> {
+                        setResult(RESULT_OK)
+                        finish()
+                    }
+                    is NetworkState.Loading -> {
+                    }
+                }
+            }
+        }
+
         settingToolbar()
+        settingReason()
         settingReportButton()
         settingErrorHandling()
     }
@@ -35,27 +56,50 @@ class ReportActivity : AppCompatActivity() {
         }
     }
 
+    private fun settingReason() {
+        with(binding) {
+            radioGroupReport.setOnCheckedChangeListener { _, checkedId ->
+                val selectedReason = when (checkedId) {
+                    R.id.radioButtonPirate -> getString(R.string.text_report_reason_pirate)
+                    R.id.radioButtonCurse -> getString(R.string.text_report_reason_curse)
+                    R.id.radioButtonPromote -> getString(R.string.text_report_reason_promote)
+                    R.id.radioButtonViolence -> getString(R.string.text_report_reason_violence)
+                    R.id.radioButtonWrong -> getString(R.string.text_report_reason_wrong)
+                    else -> getString(R.string.text_report_reason_etc)
+                }
+                viewModel.setSelectedReason(selectedReason)
+            }
+
+
+            textFieldDetailReason.addTextChangedListener { detailReason ->
+                viewModel.setDetailedReason(detailReason.toString())
+            }
+        }
+    }
+
     private fun settingReportButton() {
         with(binding) {
             buttonReport.setOnClickListener {
                 if (radioGroupReport.checkedRadioButtonId == R.id.radioButtonEtc) {
                     if (isFormValid()) {
-                        setResult(RESULT_OK)
-                        finish()
+                        viewModel.submitReportReview()
+//                        setResult(RESULT_OK)
+//                        finish()
                     }
                 } else {
-                    setResult(RESULT_OK)
-                    finish()
+                    viewModel.submitReportReview()
+//                    setResult(RESULT_OK)
+//                    finish()
                 }
             }
         }
     }
 
     private fun isFormValid(): Boolean {
-        return if (binding.textFieldEtc.text.isNullOrBlank()) {
-            binding.textInputLayoutEtc.error = getString(R.string.text_report_error_message)
-            binding.textFieldEtc.requestFocus()
-            this@ReportActivity.showSoftInput(binding.textFieldEtc)
+        return if (binding.textFieldDetailReason.text.isNullOrBlank()) {
+            binding.textInputLayoutDetailReason.error = getString(R.string.text_report_error_message)
+            binding.textFieldDetailReason.requestFocus()
+            this@ReportActivity.showSoftInput(binding.textFieldDetailReason)
             false
         } else {
             true
@@ -63,8 +107,8 @@ class ReportActivity : AppCompatActivity() {
     }
 
     private fun settingErrorHandling() {
-        binding.textFieldEtc.addTextChangedListener {
-            clearErrorMessage(binding.textInputLayoutEtc)
+        binding.textFieldDetailReason.addTextChangedListener {
+            clearErrorMessage(binding.textInputLayoutDetailReason)
         }
     }
 
