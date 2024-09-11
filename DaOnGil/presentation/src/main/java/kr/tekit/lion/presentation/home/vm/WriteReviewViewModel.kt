278a545405cddfb834fecec9ad4850dd30c7e111
Jieun <lje2000lje@naver.com>
2024-09-11 19:07:34 +0900

    [Feat] #185 리뷰 전체보기 에러 처리 뷰모델 수정

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/vm/WriteReviewViewModel.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/vm/WriteReviewViewModel.kt
@@ -6,15 +6,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kr.tekit.lion.domain.model.placereview.NewReviewData
-import kr.tekit.lion.domain.model.placereview.NewReviewImages
 import kr.tekit.lion.domain.repository.PlaceRepository
 import kr.tekit.lion.presentation.delegate.NetworkErrorDelegate
-import org.json.JSONObject
 import java.time.LocalDate
 import kr.tekit.lion.domain.exception.onError
 import kr.tekit.lion.domain.exception.onSuccess
+import kr.tekit.lion.presentation.delegate.NetworkState
 import kr.tekit.lion.presentation.home.model.NewReviewImgs
 import javax.inject.Inject
 
@@ -25,51 +25,42 @@ class WriteReviewViewModel @Inject constructor(
 
     @Inject
     lateinit var networkErrorDelegate: NetworkErrorDelegate
+    val networkState: StateFlow<NetworkState> get() = networkErrorDelegate.networkState
 
     private val _placeVisitDate = MutableLiveData<LocalDate>()
     val placeVisitDate: LiveData<LocalDate> = _placeVisitDate
 
     private val _newReviewData = MutableLiveData<NewReviewData>()
-    val newReviewData : LiveData<NewReviewData> = _newReviewData
+    val newReviewData: LiveData<NewReviewData> = _newReviewData
 
     private val _newReviewImages = MutableLiveData<NewReviewImgs>()
-    val newReviewImages : LiveData<NewReviewImgs> = _newReviewImages
+    val newReviewImages: LiveData<NewReviewImgs> = _newReviewImages
 
     fun setPlaceVisitDate(startDate: LocalDate) {
         _placeVisitDate.value = startDate
     }
 
-    fun writePlaceReviewData(placeId: Long, date: LocalDate, grade: Float, content: String, callback: (Boolean, Boolean, Int) -> Unit) {
-        var requestFlag = false
-        var code = 0
+    fun writePlaceReviewData(placeId: Long, date: LocalDate, grade: Float, content: String) {
 
         _newReviewData.value = newReviewData.value?.copy(
             date = date,
             grade = grade,
             content = content
         )
-        viewModelScope.launch{
-            val success = try {
-                newReviewImages.value?.let {
-                    writePlaceReviewRepository.writePlaceReviewData(placeId, NewReviewData(date, grade, content), it.toDomainModel())
-                        .onSuccess {
-                            Log.e("writePlaceReviewData image", "images : ${newReviewImages.value}")
-                            requestFlag = true
-                            val json = JSONObject(it.toString())
-                            val successCode = json.getInt("code")
-                            Log.d("writePlaceReviewData", json.toString())
 
-                            code = successCode
-                        }.onError {
-                            networkErrorDelegate.handleNetworkError(it)
-                        }
-                }
-                true
-            } catch (e: Exception) {
-                Log.d("writePlaceReviewData", "Error : ${e.message}")
-                false
+        viewModelScope.launch {
+            newReviewImages.value?.let {
+                writePlaceReviewRepository.writePlaceReviewData(
+                    placeId,
+                    NewReviewData(date, grade, content),
+                    it.toDomainModel()
+                )
+                    .onSuccess {
+                        networkErrorDelegate.handleNetworkSuccess()
+                    }.onError {
+                        networkErrorDelegate.handleNetworkError(it)
+                    }
             }
-            callback(success, requestFlag, code)
         }
     }
 
