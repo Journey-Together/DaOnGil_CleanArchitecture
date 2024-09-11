0243819a07cdfe2917e150890371664a5fbd1acd
Jieun <lje2000lje@naver.com>
2024-09-11 16:41:16 +0900

    [Feat] #175 관광지 리뷰 전체보기 에러 처리

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/ReviewListActivity.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/ReviewListActivity.kt
@@ -1,15 +1,20 @@
 package kr.tekit.lion.presentation.home
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import kr.tekit.lion.domain.model.placereviewlist.PlaceReview
 import kr.tekit.lion.presentation.R
 import kr.tekit.lion.presentation.databinding.ActivityReviewListBinding
+import kr.tekit.lion.presentation.delegate.NetworkState
 import kr.tekit.lion.presentation.ext.addOnScrollEndListener
 import kr.tekit.lion.presentation.ext.repeatOnStarted
 import kr.tekit.lion.presentation.ext.showSnackbar
@@ -24,13 +29,14 @@ class ReviewListActivity : AppCompatActivity() {
         ActivityReviewListBinding.inflate(layoutInflater)
     }
 
-    private val reportLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        when(result.resultCode){
-            RESULT_OK -> {
-                binding.root.showSnackbar("신고가 완료되었습니다")
+    private val reportLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            when (result.resultCode) {
+                RESULT_OK -> {
+                    binding.root.showSnackbar("신고가 완료되었습니다")
+                }
             }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,23 +47,60 @@ class ReviewListActivity : AppCompatActivity() {
         settingToolbar()
 
         repeatOnStarted {
-            viewModel.loginState.collect { uiState ->
-                when (uiState) {
-                    is LogInState.Checking -> {
-                        return@collect
-                    }
+            launch {
+                viewModel.loginState.collect { uiState ->
+                    when (uiState) {
+                        is LogInState.Checking -> {
+                            return@collect
+                        }
 
-                    is LogInState.LoggedIn -> {
-                        getReviewListInfo(placeId)
-                    }
+                        is LogInState.LoggedIn -> {
+                            getReviewListInfo(placeId)
+                        }
 
-                    is LogInState.LoginRequired -> {
-                        getReviewListInfoGuest(placeId)
+                        is LogInState.LoginRequired -> {
+                            getReviewListInfoGuest(placeId)
+                        }
+                    }
+                }
+            }
+
+            launch {
+                viewModel.networkState.collectLatest { state ->
+                    when (state) {
+                        is NetworkState.Loading -> {
+                            binding.reviewListErrorLayout.visibility = View.GONE
+                        }
+
+                        is NetworkState.Success -> {
+                            binding.reviewListErrorLayout.visibility = View.GONE
+                        }
+
+                        is NetworkState.Error -> {
+                            binding.reviewListToolbarTitleTv.setTextColor(
+                                ContextCompat.getColor(
+                                    applicationContext,
+                                    R.color.text_primary
+                                )
+                            )
+                            binding.reviewListToolbar.navigationIcon?.setTint(
+                                ContextCompat.getColor(
+                                    applicationContext,
+                                    R.color.text_primary
+                                )
+                            )
+                            binding.reviewListThumbnailDark.visibility = View.GONE
+                            binding.reviewListThumbnailIv.visibility = View.GONE
+                            binding.reviewListTitleTv.visibility = View.GONE
+                            binding.reviewListAddressTv.visibility = View.GONE
+                            binding.reviewListContentLayout.visibility = View.GONE
+                            binding.reviewListErrorLayout.visibility = View.VISIBLE
+                            binding.reviewListErrorTv.text = state.msg
+                        }
                     }
                 }
             }
         }
-
     }
 
     private fun settingToolbar() {
