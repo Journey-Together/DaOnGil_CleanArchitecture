765ff623de312b23d679b08869b2bbcfe97eddfe
chanho0908 <uni070@naver.com>
2024-09-12 12:59:07 +0900

    [Feat] #190 네트워크 상태 확인 확장함수 추가

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/ext/ContextExt.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/ext/ContextExt.kt
@@ -4,6 +4,8 @@ import android.content.ContentUris
 import android.content.Context
 import android.content.Intent
 import android.database.Cursor
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.Uri
 import android.provider.DocumentsContract
 import android.provider.MediaStore
@@ -155,6 +157,14 @@ fun Context.showInfinitySnackBar(view: View, message: String) {
         setAction("닫기") { this.dismiss() }
         show()
     }
+}
+
+fun Context.isNetworkConnected(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val network = connectivityManager.activeNetwork
+    val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+
+    return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
 }
 
 fun Context.showPhotoDialog(
