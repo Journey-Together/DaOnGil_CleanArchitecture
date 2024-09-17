1d6626038024a30cdcb9d317d0b282949aa465e8
chanho0908 <uni070@naver.com>
2024-09-17 19:19:33 +0900

    [Feat] #226 관광공사 코드 API 호출 실패시 재시도 로직 추가

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/splash/vm/SplashViewModel.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/splash/vm/SplashViewModel.kt
@@ -4,37 +4,38 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kr.tekit.lion.domain.repository.ActivationRepository
 import kr.tekit.lion.domain.usecase.areacode.InitAreaCodeInfoUseCase
+import kr.tekit.lion.domain.usecase.base.onError
 import kr.tekit.lion.domain.usecase.base.onSuccess
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val activationRepository: ActivationRepository,
+    activationRepository: ActivationRepository,
     private val initAreaCodeInfoUseCase: InitAreaCodeInfoUseCase,
 ): ViewModel() {
 
-    private val _userActivationState = MutableSharedFlow<Boolean>()
-    val userActivationState = _userActivationState.asSharedFlow()
+    private val _err = MutableSharedFlow<Boolean>()
+    val err = _err.asSharedFlow()
 
-    init {
-        viewModelScope.launch {
-            checkFirstLogIn()
-        }
-    }
-
-    private fun checkFirstLogIn() = viewModelScope.launch {
-        activationRepository.userActivation.collect{
-            _userActivationState.emit(it)
-        }
-    }
+    val userActivationState = activationRepository.userActivation.shareIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000)
+    )
 
     suspend fun whenUserActivationIsFirst(onComplete: () -> Unit){
         initAreaCodeInfoUseCase().onSuccess {
             onComplete()
+        }.onError {
+            viewModelScope.launch {
+                _err.emit(true)
+            }
         }
     }
 }
