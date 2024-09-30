af769650acccde574efdd9b76726eec5271f8baa
Boram <dev.lboram@gmail.com>
2024-09-30 16:58:09 +0900

    [Fix] #253 새로운 일정 화면으로 갔다 뒤로가기 버튼을 눌러서 돌아오는 경우 등록된 일정이 없다는 안내 문구가 사라지는 오류 해결

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/myschedule/MyScheduleActivity.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/myschedule/MyScheduleActivity.kt
@@ -43,6 +43,12 @@ class MyScheduleActivity : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_SCHEDULE_REVIEW_CANCELED) return@registerForActivityResult
 
+            // '일정 등록하러가기' 버튼을 눌렀다가 '뒤로가기 버튼'을 눌러서 취소한 경우
+            if (result.resultCode == RESULT_CANCELED) {
+                showScheduleEmptyPrompt()
+                return@registerForActivityResult
+            }
+
             // 일정 목록 갱신
             viewModel.refreshScheduleList()
             when (result.resultCode) {
