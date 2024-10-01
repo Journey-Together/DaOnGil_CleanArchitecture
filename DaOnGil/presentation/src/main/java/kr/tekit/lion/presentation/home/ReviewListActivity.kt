d533f6332c56d6211ae34481134b5a2e9b8bc6d3
Jieun <lje2000lje@naver.com>
2024-10-02 04:05:14 +0900

    [Feat] #263 관광지 리뷰 모아보기 네트워크 에러처리

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/ReviewListActivity.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/home/ReviewListActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 import kr.tekit.lion.domain.model.placereviewlist.PlaceReview
 import kr.tekit.lion.presentation.R
 import kr.tekit.lion.presentation.databinding.ActivityReviewListBinding
@@ -18,8 +20,12 @@ import kr.tekit.lion.presentation.delegate.NetworkState
 import kr.tekit.lion.presentation.ext.addOnScrollEndListener
 import kr.tekit.lion.presentation.ext.repeatOnStarted
 import kr.tekit.lion.presentation.ext.showSnackbar
+import kr.tekit.lion.presentation.ext.updateToolbarColors
+import kr.tekit.lion.presentation.ext.updateVisibility
 import kr.tekit.lion.presentation.home.adapter.ReviewListRVAdapter
 import kr.tekit.lion.presentation.home.vm.ReviewListViewModel
+import kr.tekit.lion.presentation.observer.ConnectivityObserver
+import kr.tekit.lion.presentation.observer.NetworkConnectivityObserver
 import kr.tekit.lion.presentation.splash.model.LogInState
 
 @AndroidEntryPoint
@@ -27,6 +33,9 @@ class ReviewListActivity : AppCompatActivity() {
     private val viewModel: ReviewListViewModel by viewModels()
     private val binding: ActivityReviewListBinding by lazy {
         ActivityReviewListBinding.inflate(layoutInflater)
+    }
+    private val connectivityObserver: ConnectivityObserver by lazy {
+        NetworkConnectivityObserver.getInstance(this)
     }
 
     private val reportLauncher =
@@ -47,58 +56,10 @@ class ReviewListActivity : AppCompatActivity() {
         settingToolbar()
 
         repeatOnStarted {
-            launch {
-                viewModel.loginState.collect { uiState ->
-                    when (uiState) {
-                        is LogInState.Checking -> {
-                            return@collect
-                        }
-
-                        is LogInState.LoggedIn -> {
-                            getReviewListInfo(placeId)
-                        }
-
-                        is LogInState.LoginRequired -> {
-                            getReviewListInfoGuest(placeId)
-                        }
-                    }
-                }
-            }
-
-            launch {
-                viewModel.networkState.collectLatest { state ->
-                    when (state) {
-                        is NetworkState.Loading -> {
-                            binding.reviewListErrorLayout.visibility = View.GONE
-                        }
-
-                        is NetworkState.Success -> {
-                            binding.reviewListErrorLayout.visibility = View.GONE
-                        }
-
-                        is NetworkState.Error -> {
-                            binding.reviewListToolbarTitleTv.setTextColor(
-                                ContextCompat.getColor(
-                                    applicationContext,
-                                    R.color.text_primary
-                                )
-                            )
-                            binding.reviewListToolbar.navigationIcon?.setTint(
-                                ContextCompat.getColor(
-                                    applicationContext,
-                                    R.color.text_primary
-                                )
-                            )
-                            binding.reviewListThumbnailDark.visibility = View.GONE
-                            binding.reviewListThumbnailIv.visibility = View.GONE
-                            binding.reviewListTitleTv.visibility = View.GONE
-                            binding.reviewListAddressTv.visibility = View.GONE
-                            binding.reviewListContentLayout.visibility = View.GONE
-                            binding.reviewListErrorLayout.visibility = View.VISIBLE
-                            binding.reviewListErrorTv.text = state.msg
-                        }
-                    }
-                }
+            supervisorScope {
+                launch { collectLoginState(placeId) }
+                launch { collectReviewNetworkState() }
+                launch { observeConnectivity(placeId) }
             }
         }
     }
@@ -161,4 +122,102 @@ class ReviewListActivity : AppCompatActivity() {
             settingReviewListRVAdapter(placeReviewInfo.placeReviewList, false)
         }
     }
+
+    private suspend fun collectLoginState(placeId: Long) {
+        viewModel.loginState.collect { uiState ->
+            when (uiState) {
+                is LogInState.Checking -> {
+                    return@collect
+                }
+
+                is LogInState.LoggedIn -> {
+                    getReviewListInfo(placeId)
+                }
+
+                is LogInState.LoginRequired -> {
+                    getReviewListInfoGuest(placeId)
+                }
+            }
+        }
+    }
+
+    private suspend fun collectReviewNetworkState() {
+        with(binding) {
+            viewModel.networkState.collectLatest { state ->
+                when (state) {
+                    is NetworkState.Loading -> {
+                        reviewListErrorLayout.visibility = View.GONE
+                    }
+
+                    is NetworkState.Success -> {
+                        reviewListErrorLayout.visibility = View.GONE
+                    }
+
+                    is NetworkState.Error -> {
+                        this@ReviewListActivity.updateToolbarColors(
+                            reviewListToolbarTitleTv,
+                            reviewListToolbar,
+                            R.color.text_primary
+                        )
+
+                        root.updateVisibility(
+                            isVisible = false,
+                            reviewListThumbnailDark, reviewListThumbnailIv, reviewListTitleTv,
+                            reviewListAddressTv, reviewListContentLayout
+                        )
+                        binding.reviewListErrorLayout.visibility = View.VISIBLE
+                        binding.reviewListErrorTv.text = state.msg
+                    }
+                }
+            }
+        }
+    }
+
+    private suspend fun observeConnectivity(placeId: Long) {
+        with(binding) {
+            connectivityObserver.getFlow().collect { connectivity ->
+                when (connectivity) {
+                    ConnectivityObserver.Status.Available -> {
+                        reviewListErrorLayout.visibility = View.GONE
+
+                        this@ReviewListActivity.updateToolbarColors(
+                            reviewListToolbarTitleTv,
+                            reviewListToolbar,
+                            R.color.white
+                        )
+                        root.updateVisibility(
+                            isVisible = true,
+                            reviewListThumbnailDark, reviewListThumbnailIv, reviewListTitleTv,
+                            reviewListAddressTv, reviewListContentLayout
+                        )
+
+                        if (viewModel.networkState.value is NetworkState.Error) {
+                            lifecycleScope.launch {
+                                collectLoginState(placeId)
+                            }
+                        }
+                    }
+                    // Unavailable, Losing, Lost
+                    else -> {
+                        val msg = "${getString(R.string.text_network_is_unavailable)}\n" +
+                                "${getString(R.string.text_plz_check_network)} "
+
+                        this@ReviewListActivity.updateToolbarColors(
+                            reviewListToolbarTitleTv,
+                            reviewListToolbar,
+                            R.color.text_primary
+                        )
+                        root.updateVisibility(
+                            isVisible = false,
+                            reviewListThumbnailDark, reviewListThumbnailIv, reviewListTitleTv,
+                            reviewListAddressTv, reviewListContentLayout
+                        )
+                        reviewListErrorLayout.visibility = View.VISIBLE
+                        reviewListErrorTv.text = msg
+                    }
+                }
+            }
+        }
+    }
+
 }
