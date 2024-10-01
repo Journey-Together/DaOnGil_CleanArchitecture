c0de7e70967ec691e7728cbf2da45d9197e45a42
rmadmsjm <dmswjdals12@naver.com>
2024-10-02 02:36:41 +0900

    [Feat] #259 일정 북마크 항목에 대한 접근성 설명 추가
    닉네임, 일정명 순서로 읽히도록 설정

--- DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/bookmark/adapter/PlanBookmarkRVAdapter.kt
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/bookmark/adapter/PlanBookmarkRVAdapter.kt
@@ -1,7 +1,11 @@
 package kr.tekit.lion.presentation.bookmark.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kr.tekit.lion.domain.model.PlanBookmark
@@ -59,6 +63,26 @@ class PlanBookmarkRVAdapter(
                 .placeholder(R.drawable.empty_view)
                 .error(R.drawable.empty_view)
                 .into(binding.imageViewBookmarkUserProfile)
+
+            val bookmarkBtnDescription = binding.root.context.getString(R.string.text_update_plan_bookmark, planBookmark.title)
+            binding.scheduleBookmarkBtn.contentDescription = bookmarkBtnDescription
+
+            ViewCompat.setAccessibilityDelegate(binding.root, object : AccessibilityDelegateCompat() {
+                override fun onInitializeAccessibilityNodeInfo(
+                    host: View,
+                    info: AccessibilityNodeInfoCompat
+                ) {
+                    super.onInitializeAccessibilityNodeInfo(host, info)
+
+                    val combinedDescription = StringBuilder()
+                        .append(binding.textViewScheduleBookmarkNickname.text)
+                        .append("님의, ")
+                        .append(binding.textViewScheduleBookmarkTitle.text)
+                        .append("일정")
+
+                    info.text = combinedDescription.toString()
+                }
+            })
         }
     }
 }
