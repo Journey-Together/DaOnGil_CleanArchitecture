403797df145546091ec0f0cff4631e7b08ebf507
chanho0908 <uni070@naver.com>
2024-10-09 23:58:25 +0900

    [Feat] #281 ViewModel Firebase Crashlytics 공통 예외 처리 Class

---
+++ DaOnGil/presentation/src/main/java/kr/tekit/lion/presentation/base/BaseViewModel.kt
@@ -0,0 +1,16 @@
+package kr.tekit.lion.presentation.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+open class BaseViewModel : ViewModel() {
+    protected val recordExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        viewModelScope.launch(Dispatchers.IO) {
+            FirebaseCrashlytics.getInstance().recordException(throwable)
+        }
+    }
+}
