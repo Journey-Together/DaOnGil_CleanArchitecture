f8662f6bed803e3db358fc9382a1a4143847b2d3
kangyein9892 <inwon9892@naver.com>
2024-10-17 01:29:07 +0900

    [Feat] #284 응급 메인 화면 에러 처리 기능 추가

--- DaOnGil/presentation/src/main/java/kr/techit/lion/presentation/main/fragment/EmergencyMainFragment.kt
+++ DaOnGil/presentation/src/main/java/kr/techit/lion/presentation/main/fragment/EmergencyMainFragment.kt
@@ -4,17 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 import kr.techit.lion.presentation.R
 import kr.techit.lion.presentation.databinding.FragmentEmergencyMainBinding
 import kr.techit.lion.presentation.emergency.EmergencyMapActivity
 import kr.techit.lion.presentation.emergency.PharmacyMapActivity
+import kr.techit.lion.presentation.emergency.fragment.EmergencyAreaDialog
+import kr.techit.lion.presentation.ext.repeatOnViewStarted
+import kr.techit.lion.presentation.observer.ConnectivityObserver
+import kr.techit.lion.presentation.observer.NetworkConnectivityObserver
 
 class EmergencyMainFragment : Fragment(R.layout.fragment_emergency_main) {
+
+    private val connectivityObserver: ConnectivityObserver by lazy {
+        NetworkConnectivityObserver.getInstance(requireActivity())
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentEmergencyMainBinding.bind(view)
+
 
         binding.emrAedCard.setOnClickListener {
             val intent = Intent(requireActivity(), EmergencyMapActivity::class.java)
@@ -24,6 +35,36 @@ class EmergencyMainFragment : Fragment(R.layout.fragment_emergency_main) {
         binding.pharmacyCard.setOnClickListener {
             val intent = Intent(requireActivity(), PharmacyMapActivity::class.java)
             startActivity(intent)
+        }
+
+        repeatOnViewStarted {
+            supervisorScope {
+                launch { observeConnectivity(binding) }
+            }
+        }
+    }
+
+    private suspend fun observeConnectivity(binding: FragmentEmergencyMainBinding) {
+        with(binding){
+            connectivityObserver.getFlow().collect { connectivity ->
+                when(connectivity){
+                    ConnectivityObserver.Status.Available -> {
+                        emergencyMainErrorLayout.visibility = View.GONE
+                        emergencyMainProgressBar.visibility = View.GONE
+                        emergencyMainLayout.visibility = View.VISIBLE
+                    }
+                    ConnectivityObserver.Status.Unavailable,
+                    ConnectivityObserver.Status.Losing,
+                    ConnectivityObserver.Status.Lost -> {
+                        emergencyMainErrorLayout.visibility = View.VISIBLE
+                        emergencyMainProgressBar.visibility = View.GONE
+                        emergencyMainLayout.visibility = View.GONE
+                        val msg = "${getString(R.string.text_network_is_unavailable)}\n" +
+                                "${getString(R.string.text_plz_check_network)} "
+                        emergencyMainErrorMsg.text = msg
+                    }
+                }
+            }
         }
     }
 }
