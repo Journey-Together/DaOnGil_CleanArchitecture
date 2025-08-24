4b1f1e8bd7dc22d1982418c7921408b6e6018619
chanho0908 <uni070@naver.com>
2025-08-24 11:25:00 +0900

    [refactor] #334 온보딩 페이지 데이터 모델 변경
    - 이미지, 제목, 설명, 추가 설명 필드 포함
    - 추가 설명 필드는 기본값으로 빈 문자열 설정

--- presentation/src/main/java/kr/techit/lion/presentation/login/model/OnBoardingPage.kt
+++ presentation/src/main/java/kr/techit/lion/presentation/login/model/OnBoardingPage.kt
@@ -3,8 +3,13 @@ package kr.techit.lion.presentation.login.model
 import android.graphics.drawable.Drawable
 
 data class OnBoardingPage(
-    val imageId: Drawable,
-    val text1: String,
-    val text2: String,
-    val text3: String
-)
+    val image: Drawable,
+    val title: String,
+    val description: String,
+    val extra: String = EMPTY_EXTRA,
+) {
+    companion object {
+        private const val EMPTY_EXTRA = ""
+    }
+}
+
