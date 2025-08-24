d1ebfaf7c89eb6fc505587c49529ec96b67e0eee
rmadmsjm <dmswjdals12@naver.com>
2025-08-24 18:08:44 +0900

    [Feat] #339 부모 view에만 contentDescription 설정

--- presentation/src/main/java/kr/techit/lion/presentation/main/adapter/HomeVPAdapter.kt
+++ presentation/src/main/java/kr/techit/lion/presentation/main/adapter/HomeVPAdapter.kt
@@ -2,8 +2,10 @@ package kr.techit.lion.presentation.main.adapter
 
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kr.techit.lion.presentation.R
 import kr.techit.lion.presentation.databinding.ItemHomeVpBinding
 
 class HomeVPAdapter(
@@ -33,6 +35,17 @@ class HomeVPAdapter(
         fun bind(image: Drawable, currentPage: Int) {
             binding.itemHomeIv.setImageDrawable(image)
             binding.itemHomeCountTv.text = currentPage.toString()
+
+            // 부모 view에만 contentDescription 설정
+            with(binding.root) {
+                importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_YES
+                contentDescription = when(bindingAdapterPosition) {
+                    0 -> binding.root.context.getString(R.string.banner_place_search_desc)
+                    1 -> binding.root.context.getString(R.string.banner_emergency_desc)
+                    2 -> binding.root.context.getString(R.string.banner_schedule_desc)
+                    else -> ""
+                }
+            }
 
             binding.root.setOnClickListener {
                 if (bindingAdapterPosition != RecyclerView.NO_POSITION) {
