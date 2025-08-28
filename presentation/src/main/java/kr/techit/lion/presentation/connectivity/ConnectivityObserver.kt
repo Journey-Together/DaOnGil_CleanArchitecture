56110a7b7fe86e33304b1bba1b6c76742fdb4428
chanho0908 <uni070@naver.com>
2025-08-28 22:29:07 +0900

    [feat] #334 현재 인터넷 연결 상태를 바로 반환하는 메서드 추가, Di Module 추가

--- presentation/src/main/java/kr/techit/lion/presentation/connectivity/ConnectivityObserver.kt
+++ presentation/src/main/java/kr/techit/lion/presentation/connectivity/ConnectivityObserver.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.flow.Flow
 interface ConnectivityObserver {
     fun getFlow(): Flow<Status>
 
+    fun value(): Status
+
     enum class Status{
         Unavailable, Available, Losing, Lost
     }
