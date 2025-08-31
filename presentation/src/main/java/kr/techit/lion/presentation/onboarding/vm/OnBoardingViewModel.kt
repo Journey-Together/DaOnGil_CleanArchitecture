197a4cde53ca63522a0460c636c9ac0fd4586b48
chanho0908 <uni070@naver.com>
2025-08-31 10:05:32 +0900

    [refactor] #334 OnBoardingViewModel에 @Inject 어노테이션 추가

--- presentation/src/main/java/kr/techit/lion/presentation/onboarding/vm/OnBoardingViewModel.kt
+++ presentation/src/main/java/kr/techit/lion/presentation/onboarding/vm/OnBoardingViewModel.kt
@@ -11,7 +11,7 @@ import kr.techit.lion.presentation.onboarding.model.OnBoardingUiState
 import javax.inject.Inject
 
 @HiltViewModel
-class OnBoardingViewModel : ViewModel() {
+class OnBoardingViewModel @Inject constructor() : ViewModel() {
     private val _uiState = MutableStateFlow(OnBoardingUiState())
     val uiState: StateFlow<OnBoardingUiState> get() = _uiState.asStateFlow()
 
