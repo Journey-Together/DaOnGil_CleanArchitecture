a610c5e48eff47be491ef391db4698cf1efb260f
Jieun <lje2000lje@naver.com>
2025-09-17 23:29:49 +0900

    [fix] login response data에서 name nullable로 수정

--- data/src/main/java/kr/techit/lion/data/dto/response/signin/Data.kt
+++ data/src/main/java/kr/techit/lion/data/dto/response/signin/Data.kt
@@ -8,7 +8,7 @@ internal data class Data(
     val loginType: String,
     val memberId: Int,
     val memberType: String,
-    val name: String,
+    val name: String?,
     val profileUuid: String,
     val refreshToken: String,
     val accessToken: String,
